fe282c2b7d250c78fe0c73267759373efb791f18
Donkoko <nbonev@duck.com>
2023-02-15 20:40:08 +0100

    solving issue with vtest

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -60,11 +60,11 @@ const handlers = [
   ),
   rest.post(
     `${SUPABASE_URL}${SUPABASE_AUTH_ADMIN_USER_API}`,
-    async (res, ctx) => res(ctx.status(200), ctx.json(authAccount))
+    async (req, res, ctx) => res(ctx.status(200), ctx.json(authAccount))
   ),
   rest.delete(
     `${SUPABASE_URL}${SUPABASE_AUTH_ADMIN_USER_API}/*`,
-    async (res, ctx) => res(ctx.status(200), ctx.json({}))
+    async (req, res, ctx) => res(ctx.status(200), ctx.json({}))
   ),
 ];
 
