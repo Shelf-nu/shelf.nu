aad292fa7799e4ac4b36fe518d318c902d1a0d97
Donkoko <nbonev@duck.com>
2023-03-15 22:56:43 +0200

    adjusting some of the layout design

--- app/routes/_layout+/_layout.tsx
+++ app/routes/_layout+/_layout.tsx
@@ -22,15 +22,17 @@ export default function App() {
   const { user } = useLoaderData<typeof loader>();
 
   return (
-    <div className="flex h-full min-h-screen flex-col">
-      <main className="flex h-full bg-white">
-        <div className="h-full w-80 border-r bg-primary p-8">
+    <div className="flex h-full min-h-screen flex-col ">
+      <main className="flex h-full bg-primary ">
+        <div className="h-full w-80  p-8">
           <Sidebar user={user} />
         </div>
 
-        <div className="flex h-full min-h-screen flex-1 flex-col bg-primary-25 p-8">
-          <Header />
-          <Outlet />
+        <div className="h-full w-full py-3">
+          <div className="flex h-full flex-1 flex-col rounded-l-[40px] bg-primary-25 p-8">
+            <Header />
+            <Outlet />
+          </div>
         </div>
       </main>
     </div>
