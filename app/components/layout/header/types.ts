43a9a3ee4b349d22e00f57a8eb49fbdd4eee201a
Donkoko <nbonev@duck.com>
2023-03-15 17:49:19 +0200

    fixing typescript error

--- app/components/layout/header/types.ts
+++ app/components/layout/header/types.ts
@@ -3,52 +3,51 @@
  * - title
  * - subtitle
  * - actions
- * 
+ *
  * Those will be rendered on on top of the layout
  */
 
 import type { LinkProps } from "@remix-run/react";
 
 export interface HeaderData {
-	/** Heading/title that will be rendered on top of the view */
-	title: string;
+  /** Heading/title that will be rendered on top of the view */
+  title: string;
 
-	/** Subheading rendered below the heading */
-	subHeading: string;
+  /** Subheading rendered below the heading */
+  subHeading?: string;
 
-	/** Array of the actions that will be rendered on the left side of the view */
-	actions: Action[]
+  /** Array of the actions that will be rendered on the left side of the view */
+  actions: Action[];
 }
 
 export type Action = {
-	/** Name of the component that should be rendered */
-	component: string;
+  /** Name of the component that should be rendered */
+  component: string;
 
+  /** Props to be passed to the component */
+  props: LinkProps & {
+    /** Optional id used for testing it Cypress */
+    "data-test-id"?: string;
 
-	/** Props to be passed to the component */
-	props: LinkProps & {
-		/** Optional id used for testing it Cypress */
-		"data-test-id"?: string
+    /** The possible options for icons to be rendered in the button */
+    icon: ButtonIcon;
 
-		/** The possible options for icons to be rendered in the button */
-		icon: ButtonIcon;
+    /** The button variant. Default is primary */
+    variant?: ButtonVariant;
 
-		/** The button variant. Default is primary */
-		variant?: ButtonVariant
+    /** Width of the button. Default is auto */
+    width?: ButtonWidth;
+  };
 
-		/** Width of the button. Default is auto */
-		width?: ButtonWidth;
-	};
-
-	/** Children to be rendered inside the component. Can only be a string as components are not serializable so they cannot be returned as part of the json response */
-	children: string;
+  /** Children to be rendered inside the component. Can only be a string as components are not serializable so they cannot be returned as part of the json response */
+  children: string;
 };
 
 /** The possible options for icons to be rendered in the button */
-export type ButtonIcon = 'plus' | "trash" | "archive"
+export type ButtonIcon = "plus" | "trash" | "archive";
 
 /** The button variant. Default is primary */
-export type ButtonVariant = "primary" | "secondary" | "tertiary"
+export type ButtonVariant = "primary" | "secondary" | "tertiary";
 
 /** Width of the button. Default is auto */
-export type ButtonWidth = "auto" | "full"
+export type ButtonWidth = "auto" | "full";
--- app/routes/_layout+/settings.index.tsx
+++ app/routes/_layout+/settings.index.tsx
@@ -1,5 +1,8 @@
 import { redirect } from "react-router";
 
+/** We dont render anything on /settings
+ * We just redirect to default subroute which is user
+ */
 export async function loader() {
   return redirect("user");
 }
