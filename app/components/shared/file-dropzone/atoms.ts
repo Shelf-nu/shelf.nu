4dbbb5bea165529d227f6cfeeffd5022847f2ad7
Donkoko <nbonev@duck.com>
2023-04-11 23:26:52 +0300

    color & file dropzone changed to jotai

---
+++ app/components/shared/file-dropzone/atoms.ts
@@ -0,0 +1,39 @@
+import { atom } from "jotai";
+import { formatBytes } from "~/utils";
+
+export type FileInfo = {
+  filename: string;
+  message: string;
+  error?: boolean;
+};
+
+export const fileInfoAtom = atom<FileInfo>({
+  filename: "",
+  message: "",
+  error: false,
+});
+
+export const derivedFileInfoAtom = atom(
+  (get) => get(fileInfoAtom),
+  (_get, set, fileInfo: FileInfo) => {
+    if (fileInfo) {
+      set(fileInfoAtom, {
+        filename: fileInfo.filename,
+        message: fileInfo.message,
+        error: fileInfo.error || false,
+      });
+    }
+  }
+);
+
+/** Used when uploading. Its done in a seperate atom so we can seperate the logic */
+export const updateFileInfoFromFileAtom = atom(
+  (get) => get(derivedFileInfoAtom),
+  (_get, set, file: File) => {
+    const { name, size } = file;
+    set(derivedFileInfoAtom, {
+      filename: name,
+      message: formatBytes(size),
+    });
+  }
+);
