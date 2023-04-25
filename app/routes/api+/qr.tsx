975fd8f222288c4471d7256450b261bcef62e1d8
Donkoko <nbonev@duck.com>
2023-04-26 01:04:44 +0300

    testing qr code generation

---
+++ app/routes/api+/qr.tsx
@@ -0,0 +1,39 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import QRCode from "qrcode-generator";
+
+export async function loader() {
+  // Create a QR code with a URL
+  const qr = QRCode(0, "M");
+  qr.addData("https://app.shelf.nu/q?c=clgw8cbnu0004naor12fhetbq");
+  qr.make();
+  const codeBase64 = qr.createDataURL(8);
+
+  const qrInABuffer = Buffer.from(codeBase64.split(",")[1], "base64");
+
+  const bufferSize = Buffer.byteLength(qrInABuffer);
+  const qrDataUrl = `data:image/png;base64,${qrInABuffer.toString("base64")}`;
+
+  return json({
+    qrInABuffer,
+    codeBase64,
+    qrDataUrl,
+    bufferSize,
+  });
+}
+
+export default function QRPreview() {
+  const data = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <div>Original</div>
+      <img src={data.codeBase64} alt="" />
+
+      <div>Re-generated</div>
+      <img src={data.qrDataUrl} alt="" />
+
+      <div>Buffer size</div>
+      <div>{data.bufferSize / 1024}KB</div>
+    </div>
+  );
+}
--- app/routes/api+/sse.notification.ts
+++ app/routes/api+/sse.notification.ts
@@ -1,9 +1,12 @@
 import type { LoaderArgs } from "@remix-run/node";
 
 import { eventStream } from "remix-utils";
+import { requireAuthSession } from "~/modules/auth";
 import emitter from "~/utils/emitter/emitter.server";
 
 export async function loader({ request }: LoaderArgs) {
+  await requireAuthSession(request);
+
   return eventStream(request.signal, function setup(send) {
     /** Notification is a strigified json object with the shape {@link Notification} */
     function handle(notification: string) {
