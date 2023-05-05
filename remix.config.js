709fcd34e711f8b37b45b47104162db1f4b4269e
Donkoko <nbonev@duck.com>
2023-05-05 11:52:07 +0300

    fixing remix config

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,16 @@
-import { flatRoutes } from "remix-flat-routes";
+const { flatRoutes } = require("remix-flat-routes");
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 
-export const ignoredRouteFiles = ["**/.*"];
-export async function routes(defineRoutes) {
-  return flatRoutes("routes", defineRoutes);
-}
-export const future = {
-  unstable_tailwind: true,
-  v2_meta: true,
-  v2_routeConvention: true,
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  routes: async (defineRoutes) => {
+    return flatRoutes("routes", defineRoutes);
+  },
+  future: {
+    unstable_tailwind: true,
+    v2_meta: true,
+    v2_routeConvention: true,
+  },
 };
