cb400852ee4b1dc0779b022c3c81b3d855ff16ee
Donkoko <nbonev@duck.com>
2023-07-05 17:53:35 +0300

    fixing issues with shadcdn components display name due to TS update

--- app/components/forms/select.tsx
+++ app/components/forms/select.tsx
@@ -14,89 +14,94 @@ const SelectValue = SelectPrimitive.Value;
 const SelectTrigger = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <SelectPrimitive.Trigger
-    ref={ref}
-    className={tw(
-      "flex h-10 w-full items-center justify-between rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm text-gray-500 placeholder:text-gray-500 focus:border-primary-300 focus:outline-none focus:ring-2 focus:ring-primary-25 focus:ring-offset-2  disabled:opacity-50 ",
-      className
-    )}
-    {...props}
-  >
-    {children}
-
-    <ChevronRight className="rotate-90" />
-  </SelectPrimitive.Trigger>
-));
-SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
-
-const SelectContent = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, ...props }, ref) => (
-  <SelectPrimitive.Portal>
-    <SelectPrimitive.Content
+>(function SelectTrigger({ className, children, ...props }, ref) {
+  return (
+    <SelectPrimitive.Trigger
       ref={ref}
       className={tw(
-        " relative z-50 overflow-hidden rounded-lg border border-gray-300 bg-white  p-3  shadow-md animate-in fade-in-80",
+        "focus:border-primary-300 focus:ring-primary-25 flex h-10 w-full items-center justify-between rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm text-gray-500 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2  disabled:opacity-50 ",
         className
       )}
       {...props}
     >
-      <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
-    </SelectPrimitive.Content>
-  </SelectPrimitive.Portal>
-));
-SelectContent.displayName = SelectPrimitive.Content.displayName;
+      {children}
+
+      <ChevronRight className="rotate-90" />
+    </SelectPrimitive.Trigger>
+  );
+});
+
+const SelectContent = React.forwardRef<
+  React.ElementRef<typeof SelectPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
+>(function SelectContent({ className, children, ...props }, ref) {
+  return (
+    <SelectPrimitive.Portal>
+      <SelectPrimitive.Content
+        ref={ref}
+        className={tw(
+          " animate-in fade-in-80 relative z-50 overflow-hidden rounded-lg border  border-gray-300  bg-white p-3 shadow-md",
+          className
+        )}
+        {...props}
+      >
+        <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
+      </SelectPrimitive.Content>
+    </SelectPrimitive.Portal>
+  );
+});
 
 const SelectLabel = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Label>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Label>
->(({ className, ...props }, ref) => (
-  <SelectPrimitive.Label
-    ref={ref}
-    className={tw("py-1.5 pl-8 pr-2", className)}
-    {...props}
-  />
-));
-SelectLabel.displayName = SelectPrimitive.Label.displayName;
+>(function SelectLabel({ className, ...props }, ref) {
+  return (
+    <SelectPrimitive.Label
+      ref={ref}
+      className={tw("py-1.5 pl-8 pr-2", className)}
+      {...props}
+    />
+  );
+});
 
 const SelectItem = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
->(({ className, children, ...props }, ref) => (
-  <SelectPrimitive.Item
-    ref={ref}
-    className={tw(
-      "relative flex cursor-default select-none items-center rounded-lg p-1 text-sm font-medium outline-none focus:bg-gray-50 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 ",
-      className
-    )}
-    {...props}
-  >
-    <div className="flex w-full items-center justify-between">
-      <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+>(function SelectItem({ className, children, ...props }, ref) {
+  return (
+    <SelectPrimitive.Item
+      ref={ref}
+      className={tw(
+        "relative flex cursor-default select-none items-center rounded-lg p-1 text-sm font-medium outline-none focus:bg-gray-50 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 ",
+        className
+      )}
+      {...props}
+    >
+      <div className="flex w-full items-center justify-between">
+        <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
 
-      <span className="flex h-3.5 w-3.5 items-center justify-center text-primary">
-        <SelectPrimitive.ItemIndicator>
-          <CheckIcon />
-        </SelectPrimitive.ItemIndicator>
-      </span>
-    </div>
-  </SelectPrimitive.Item>
-));
-SelectItem.displayName = SelectPrimitive.Item.displayName;
+        <span className="text-primary flex h-3.5 w-3.5 items-center justify-center">
+          <SelectPrimitive.ItemIndicator>
+            <CheckIcon />
+          </SelectPrimitive.ItemIndicator>
+        </span>
+      </div>
+    </SelectPrimitive.Item>
+  );
+});
 
 const SelectSeparator = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Separator>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Separator>
->(({ className, ...props }, ref) => (
-  <SelectPrimitive.Separator
-    ref={ref}
-    className={tw("-mx-1 my-1 h-px bg-slate-100 ", className)}
-    {...props}
-  />
-));
-SelectSeparator.displayName = SelectPrimitive.Separator.displayName;
+>(function SelectSeparator({ className, ...props }, ref) {
+  return (
+    <SelectPrimitive.Separator
+      ref={ref}
+      className={tw("-mx-1 my-1 h-px bg-slate-100 ", className)}
+      {...props}
+    />
+  );
+});
 
 export {
   Select,
--- app/components/shared/tabs.tsx
+++ app/components/shared/tabs.tsx
@@ -9,43 +9,46 @@ const Tabs = TabsPrimitive.Root;
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.List
-    ref={ref}
-    className={tw(
-      "inline-flex items-center justify-center rounded-md bg-slate-100 p-1",
-      className
-    )}
-    {...props}
-  />
-));
-TabsList.displayName = TabsPrimitive.List.displayName;
+>(function TabsList({ className, ...props }, ref) {
+  return (
+    <TabsPrimitive.List
+      ref={ref}
+      className={tw(
+        "inline-flex items-center justify-center rounded-md bg-slate-100 p-1",
+        className
+      )}
+      {...props}
+    />
+  );
+});
 
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Trigger
-    className={tw(
-      "inline-flex min-w-[100px] items-center justify-center rounded-[0.185rem] px-2.5 py-1  text-text-sm font-medium text-gray-700 transition-all  disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-900 data-[state=active]:shadow-sm ",
-      className
-    )}
-    {...props}
-    ref={ref}
-  />
-));
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+>(function TabsTrigger({ className, ...props }, ref) {
+  return (
+    <TabsPrimitive.Trigger
+      className={tw(
+        "text-text-sm inline-flex min-w-[100px] items-center justify-center rounded-[0.185rem] px-2.5  py-1 font-medium text-gray-700 transition-all  disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-900 data-[state=active]:shadow-sm ",
+        className
+      )}
+      {...props}
+      ref={ref}
+    />
+  );
+});
 
 const TabsContent = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content
-    className={tw("mt-2 rounded-lg", className)}
-    {...props}
-    ref={ref}
-  />
-));
-TabsContent.displayName = TabsPrimitive.Content.displayName;
+>(function TabsContent({ className, ...props }, ref) {
+  return (
+    <TabsPrimitive.Content
+      className={tw("mt-2 rounded-lg", className)}
+      {...props}
+      ref={ref}
+    />
+  );
+});
 
 export { Tabs, TabsList, TabsTrigger, TabsContent };
