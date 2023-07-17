4fddffbcc5d75ab66462bf8289f488f91fd33834
Donkoko <nbonev@duck.com>
2023-07-17 16:22:05 +0300

    adjusting tests so they work when running on slower github actions hardware

--- test/fixtures/account.ts
+++ test/fixtures/account.ts
@@ -27,45 +27,47 @@ export const test = base.extend<{}, { account: Account }>({
       await page.goto("/");
       await page.click("[data-test-id=signupButton]");
       await expect(page).toHaveURL(/.*join/);
-      // await page.fill("#magic-link", email);
+      await page.fill("#magic-link", email);
 
-      // await page.click("[data-test-id=continueWithMagicLink]");
-      // await page.waitForTimeout(2000);
+      await page.click("[data-test-id=continueWithMagicLink]");
+      await page.waitForTimeout(2000);
 
-      // await expect(page.getByText("Check your emails")).toBeVisible();
+      await expect(page.getByText("Check your emails")).toBeVisible();
 
-      // await page.goto("https://ethereal.email/login");
+      await page.goto("https://ethereal.email/login");
 
-      // await page.fill("#address", email);
-      // await page.fill("#password", password);
-      // await page.getByRole("button", { name: "Log in" }).click();
-      // await page.waitForTimeout(1000);
+      await page.fill("#address", email);
+      await page.fill("#password", password);
+      await page.getByRole("button", { name: "Log in" }).click();
+      await page.waitForTimeout(1000);
 
-      // await page.getByRole("link", { name: "Messages" }).click();
-      // await page.getByRole("link", { name: "Confirm Your Signup" }).click();
-      // const text = await page.innerText("#plaintext");
-      // const regex = /\[([^\]]+)\]/;
-      // const matches = text.match(regex);
-      // let confirmUrl = "";
-      // if (matches && matches.length > 0) {
-      //   confirmUrl = matches[1];
-      // }
+      await page.getByRole("link", { name: "Messages" }).click();
+      await page.getByRole("link", { name: "Confirm Your Signup" }).click();
+      const text = await page.innerText("#plaintext");
+      const regex = /\[([^\]]+)\]/;
+      const matches = text.match(regex);
+      let confirmUrl = "";
+      if (matches && matches.length > 0) {
+        confirmUrl = matches[1];
+      }
 
-      // await page.goto(confirmUrl);
+      await page.goto(confirmUrl);
 
-      // /** Fill in onboarding form */
-      // await page.fill('[data-test-id="firstName"]', firstName);
-      // await page.fill('[data-test-id="lastName"]', lastName);
-      // await page.fill('[data-test-id="password"]', password);
-      // await page.fill('[data-test-id="confirmPassword"]', password); // We use the same password that nodemailer generated for the email account
+      // Wait for the field to be present on the DOM before filling it
+      await page.waitForSelector('[data-test-id="firstName"]');
 
-      // await page.locator('[data-test-id="onboard"]').click();
-      // await expect(page).toHaveURL(/.*assets/);
-      // await expect(page.getByText("No assets on database")).toBeVisible();
-      // await page.click('[data-test-id="logout"]');
-      // await expect(page).toHaveURL(/.*login/);
+      await page.fill('[data-test-id="firstName"]', firstName);
+      await page.fill('[data-test-id="lastName"]', lastName);
+      await page.fill('[data-test-id="password"]', password);
+      await page.fill('[data-test-id="confirmPassword"]', password); // We use the same password that nodemailer generated for the email account
 
-      // // Use the account value.
+      await page.locator('[data-test-id="onboard"]').click();
+      await expect(page).toHaveURL(/.*assets/);
+      await expect(page.getByText("No assets on database")).toBeVisible();
+      await page.click('[data-test-id="logout"]');
+      await expect(page).toHaveURL(/.*login/);
+
+      // Use the account value.
       await use({ email, password, firstName, lastName });
     },
     { scope: "worker" },
@@ -75,10 +77,10 @@ export const test = base.extend<{}, { account: Account }>({
     const { email, password } = account;
     await page.goto("/");
 
-    // await page.fill('[data-test-id="email"]', email);
-    // await page.fill('[data-test-id="password"]', password);
-    // await page.click('[data-test-id="login"]');
-    // await expect(page).toHaveURL(/.*assets/);
+    await page.fill('[data-test-id="email"]', email);
+    await page.fill('[data-test-id="password"]', password);
+    await page.click('[data-test-id="login"]');
+    await expect(page).toHaveURL(/.*assets/);
 
     // Use signed-in page in the test.
     await use(page);
