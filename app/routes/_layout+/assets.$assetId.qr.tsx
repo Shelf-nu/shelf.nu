ad39b6743aa0ef158bd2435176a78cbee587e083
Hans Tognon <learningway403@gmail.com>
2023-08-31 07:05:03 +0100

    wip: reproduction

--- app/routes/_layout+/assets.$assetId.qr.tsx
+++ app/routes/_layout+/assets.$assetId.qr.tsx
@@ -65,16 +65,17 @@ export default function QRPreview() {
             {asset.title}
           </h6>
         </div>
-        <figure className="qr-code mx-auto flex h-max bg-gray-100 p-1.5">
-          <div className="bg-white">
+        <figure className="qr-code mx-auto flex h-max items-center justify-center bg-gray-100 p-1.5">
+          <div className="grow-1 flex flex-col items-center justify-center bg-white">
             <img src={data.qr.src} alt={`${data.qr.size}-shelf-qr-code.png`} />
-            <div className="flex justify-center">
+            <div className="mb-1 flex items-center justify-center">
               <span className="text-[.7rem] text-gray-600">Powered by</span>
               <img
                 src="/images/shelf-icon-28px.png"
                 alt="Shelf Logo"
-                className="h-4 w-4"
+                className="mx-0.5 h-4 w-4"
               />
+              <span className="text-[.7rem] font-medium text-black">shelf</span>
             </div>
           </div>
         </figure>
