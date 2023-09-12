8394aeb76b067ffaff8e7b1b5dfb4c736a97c233
Mahendra <hegdemahendra9@gmail.com>
2023-09-12 17:40:55 -0300

    improve:allow search when asset name has underscore and hyphen

--- app/modules/asset/service.server.ts
+++ app/modules/asset/service.server.ts
@@ -82,8 +82,7 @@ export async function getAssets({
   if (search) {
     const words = search
       .split(" ")
-      .map((w) => w.replace(/[^a-zA-Z0-9]/g, ""))
-      .filter(Boolean) //remove any special character
+      .map((w) => w.replace(/[^a-zA-Z0-9\-\_]/g, '')).filter(Boolean) //remove uncommon special character
       .join(" & ");
     where.searchVector = {
       search: words,
@@ -177,14 +176,14 @@ export async function createAsset({
     qr && qr.userId === userId && qr.assetId === null
       ? { connect: { id: qrId } }
       : {
-          create: [
-            {
-              version: 0,
-              errorCorrection: ErrorCorrection["L"],
-              user,
-            },
-          ],
-        };
+        create: [
+          {
+            version: 0,
+            errorCorrection: ErrorCorrection["L"],
+            user,
+          },
+        ],
+      };
 
   /** Data object we send via prisma to create Asset */
   const data = {
