4c5b3cf6b4e83f946a256e702ba0f43d8088310e
hunar <hunar@netling.digital>
2023-09-18 18:28:54 +0530

    handling use case if the cookies are cleared

--- app/utils/cookies.server.ts
+++ app/utils/cookies.server.ts
@@ -12,7 +12,10 @@ export async function updateCookieWithPerPage(
   const cookieHeader = request.headers.get("Cookie");
 
   let cookie = (await userPrefs.parse(cookieHeader)) || {};
-
+  /** If the cookie doesn't have perPage, adding perPage attribute and setting it to its default value 20*/
+  if (!cookie.perPage) {
+    cookie.perPage = 20;
+  }
   /** If the perPageParam is different from the cookie, we update the cookie */
   if (cookie && perPageParam !== cookie.perPage && perPageParam !== 0) {
     cookie.perPage = perPageParam;
