7e6de8029529792356edb0b3b7bd76e019bdfa28
Donkoko <nbonev@duck.com>
2023-10-17 10:36:15 +0300

    small ui updates

--- app/routes/_layout+/settings.team.tsx
+++ app/routes/_layout+/settings.team.tsx
@@ -113,11 +113,9 @@ export default function WorkspacePage() {
       </div>
       <div className="mb-6 flex gap-16">
         <div className="w-1/4">
-          <div className="text-text-sm font-medium text-gray-700">
-            Team members
-          </div>
+          <div className="text-text-sm font-medium text-gray-700">Users</div>
           <p className="text-sm text-gray-600">
-            Team members are part of your workspace but do not have an account.
+            User linked to your workspace.
           </p>
         </div>
         <div className="flex flex-1 flex-col gap-2">
@@ -170,6 +168,70 @@ export default function WorkspacePage() {
                       </ListItem>
                     ))}
                   </tbody>
+                </Table>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="mb-6 flex gap-16">
+        <div className="w-1/4">
+          <div className="text-text-sm font-medium text-gray-700">
+            Team members
+          </div>
+          <p className="text-sm text-gray-600">
+            Team members are part of your workspace but do not have an account.
+          </p>
+        </div>
+        <div className="flex flex-1 flex-col gap-2">
+          <div
+            className={tw(
+              "-mx-4 overflow-x-auto border border-gray-200  bg-white md:mx-0 md:rounded-[12px]"
+            )}
+          >
+            {!hasItems ? (
+              <EmptyState
+                customContent={{
+                  title: "No team members on database",
+                  text: "What are you waiting for? Add your first team member now!",
+                  newButtonRoute: `add-member`,
+                  newButtonContent: "Add team member",
+                }}
+              />
+            ) : (
+              <>
+                <Table>
+                  <ListHeader
+                    children={
+                      <>
+                        <Th className="hidden md:table-cell">
+                          <Button variant="primary" to={`add-member`}>
+                            <span className=" whitespace-nowrap">
+                              Add team member
+                            </span>
+                          </Button>
+                        </Th>
+                      </>
+                    }
+                  />
+                  {/* <tbody>
+                    {items.map((item) => (
+                      <ListItem item={item} key={item.id}>
+                        <>
+                          <Td className="w-full">
+                            <div className="flex items-center justify-between">
+                              <span className="text-text-sm font-medium text-gray-900">
+                                {item.name}
+                              </span>
+                            </div>
+                          </Td>
+                          <Td className="text-right">
+                            <ActionsDropdown teamMember={item} />
+                          </Td>
+                        </>
+                      </ListItem>
+                    ))}
+                  </tbody> */}
                 </Table>
               </>
             )}
