75e0f524b01b07c3bfcce74fec99e08b6b263977
hunar <hunar@netling.digital>
2023-11-06 13:58:48 +0530

    resolved location and custom field title bug

--- app/atoms/custom-fields.new.ts
+++ app/atoms/custom-fields.new.ts
@@ -1,7 +1,10 @@
 import type { ChangeEvent } from "react";
 import { atom } from "jotai";
 
-export const titleAtom = atom("Untitled custom field");
+export const titleAtom = atom("");
+titleAtom.onMount = (setAtom) => {
+  setAtom("Untitled custom field");
+};
 export const updateTitleAtom = atom(
   null,
   (_get, set, event: ChangeEvent<HTMLInputElement>) =>
--- app/atoms/locations.new.ts
+++ app/atoms/locations.new.ts
@@ -1,7 +1,11 @@
 import type { ChangeEvent } from "react";
 import { atom } from "jotai";
 
-export const titleAtom = atom("Untitled location");
+export const titleAtom = atom("");
+titleAtom.onMount = (setAtom) => {
+  setAtom("Untitled location");
+};
+
 export const updateTitleAtom = atom(
   null,
   (_get, set, event: ChangeEvent<HTMLInputElement>) =>
