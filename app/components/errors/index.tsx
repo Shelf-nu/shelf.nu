442efa0addbb8b377c767b12c29e48e60f0b2212
Donkoko <nbonev@duck.com>
2023-11-07 17:36:46 +0200

    attempting another approach

--- app/components/errors/index.tsx
+++ app/components/errors/index.tsx
@@ -3,21 +3,30 @@ import { isShelfStackError } from "~/utils/error";
 
 export const ErrorContent = () => {
   const response = useRouteError();
-  let title = "Oops, something went wrong";
-  let message =
-    "An error has occured. Please refresh the page and try again. If the issue persists, please contact support.";
 
   if (isShelfStackError(response)) {
-    title = response?.title || title;
-    message = response.message;
+    return (
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="flex flex-col items-center text-center">
+          <img src="/images/error-icon.svg" alt="" className="mb-5" />
+          <h2 className="mb-2">{response.title}</h2>
+          <p className="max-w-[550px]">{response.message}</p>
+
+          <div className=" mt-8 flex gap-3"></div>
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="flex flex-col items-center text-center">
-        <img src="/images/error-icon.svg" alt={title} className="mb-5" />
-        <h2 className="mb-2">{title}</h2>
-        <p className="max-w-[550px]">{message}</p>
+        <img src="/images/error-icon.svg" alt="" className="mb-5" />
+        <h2 className="mb-2">Oops, something went wrong</h2>
+        <p className="max-w-[550px]">
+          An error has occured. Please refresh the page and try again. If the
+          issue persists, please contact support.
+        </p>
 
         <div className=" mt-8 flex gap-3"></div>
       </div>
