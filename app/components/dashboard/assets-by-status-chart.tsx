60deea0067af75539d741afbcd15116454d0c404
hunar <hunar@netling.digital>
2023-11-09 20:15:12 +0530

    fixed resizeObserver error

--- app/components/dashboard/assets-by-status-chart.tsx
+++ app/components/dashboard/assets-by-status-chart.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "@remix-run/react";
 import { DonutChart } from "@tremor/react";
+import { ClientOnly } from "remix-utils/client-only";
 import type { loader } from "~/routes/_layout+/dashboard";
 import { Badge } from "../shared";
 import { InfoTooltip } from "../shared/info-tooltip";
@@ -10,52 +11,56 @@ export default function AssetsByStatusChart() {
   const { chartData, availableAssets, inCustodyAssets } = assetsByStatus;
 
   return (
-    <div className="mb-4 w-full rounded border border-gray-200 lg:w-1/2">
-      <div className="flex items-center justify-between">
-        <div className="flex-1 border-b p-4 text-left text-[14px] font-semibold  text-gray-900 md:px-6">
-          Assets by status
-        </div>
-        <div className="border-b p-4 text-right text-[14px] font-semibold  text-gray-900 md:px-6">
-          <InfoTooltip
-            content={
-              <>
-                <h6>Assets by Status</h6>
-                <p>
-                  Below graph shows how many percent of assets are in which
-                  status{" "}
-                </p>
-              </>
-            }
-          />
-        </div>
-      </div>
-      <div className="p-8">
-        <div className="flex gap-4">
-          <DonutChart
-            className="mt-6"
-            data={chartData}
-            category="assets"
-            index="status"
-            colors={["green", "blue"]}
-            showAnimation={true}
-            animationDuration={400}
-          />
-          <div className="min-w-[120px]">
-            <ul className="chart-items">
-              <li>
-                <Badge color="#22c55e" noBg>
-                  <strong>{availableAssets}</strong> Available
-                </Badge>
-              </li>
-              <li>
-                <Badge color="#3b82f6" noBg>
-                  <strong>{inCustodyAssets}</strong> In custody
-                </Badge>
-              </li>
-            </ul>
+    <ClientOnly fallback={null}>
+      {() => (
+        <div className="mb-4 w-full rounded border border-gray-200 lg:w-1/2">
+          <div className="flex items-center justify-between">
+            <div className="flex-1 border-b p-4 text-left text-[14px] font-semibold  text-gray-900 md:px-6">
+              Assets by status
+            </div>
+            <div className="border-b p-4 text-right text-[14px] font-semibold  text-gray-900 md:px-6">
+              <InfoTooltip
+                content={
+                  <>
+                    <h6>Assets by Status</h6>
+                    <p>
+                      Below graph shows how many percent of assets are in which
+                      status{" "}
+                    </p>
+                  </>
+                }
+              />
+            </div>
+          </div>
+          <div className="p-8">
+            <div className="flex gap-4">
+              <DonutChart
+                className="mt-6"
+                data={chartData}
+                category="assets"
+                index="status"
+                colors={["green", "blue"]}
+                showAnimation={true}
+                animationDuration={400}
+              />
+              <div className="min-w-[120px]">
+                <ul className="chart-items">
+                  <li>
+                    <Badge color="#22c55e" noBg>
+                      <strong>{availableAssets}</strong> Available
+                    </Badge>
+                  </li>
+                  <li>
+                    <Badge color="#3b82f6" noBg>
+                      <strong>{inCustodyAssets}</strong> In custody
+                    </Badge>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </ClientOnly>
   );
 }
--- app/components/dashboard/assets-for-each-month.tsx
+++ app/components/dashboard/assets-for-each-month.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "@remix-run/react";
 import { AreaChart, Card, Title } from "@tremor/react";
+import { ClientOnly } from "remix-utils/client-only";
 import type { loader } from "~/routes/_layout+/dashboard";
 import { InfoTooltip } from "../shared/info-tooltip";
 
@@ -7,42 +8,44 @@ export default function AssetsForEachMonth() {
   const { totalAssetsAtEndOfEachMonth, totalAssets } =
     useLoaderData<typeof loader>();
   return (
-    <>
-      <Card className="mb-10">
-        <Title>
-          <div className="flex justify-between">
-            <div>
-              <span className="mb-2 block text-[14px] font-medium">
-                Total inventory
-              </span>
-              <span className="block text-[30px] font-semibold text-gray-900">
-                {totalAssets} assets
-              </span>
+    <ClientOnly fallback={null}>
+      {() => (
+        <Card className="mb-10">
+          <Title>
+            <div className="flex justify-between">
+              <div>
+                <span className="mb-2 block text-[14px] font-medium">
+                  Total inventory
+                </span>
+                <span className="block text-[30px] font-semibold text-gray-900">
+                  {totalAssets} assets
+                </span>
+              </div>
+              <InfoTooltip
+                content={
+                  <>
+                    <h6>Total inventory</h6>
+                    <p>
+                      Below graph shows the total assets you have created in the
+                      last year
+                    </p>
+                  </>
+                }
+              />
             </div>
-            <InfoTooltip
-              content={
-                <>
-                  <h6>Total inventory</h6>
-                  <p>
-                    Below graph shows the total assets you have created in the
-                    last year
-                  </p>
-                </>
-              }
-            />
-          </div>
-        </Title>
-        <AreaChart
-          className="mt-4 h-72"
-          data={totalAssetsAtEndOfEachMonth}
-          index="month"
-          categories={["Total assets"]}
-          colors={["orange"]}
-          showAnimation={true}
-          animationDuration={400}
-          curveType="monotone"
-        />
-      </Card>
-    </>
+          </Title>
+          <AreaChart
+            className="mt-4 h-72"
+            data={totalAssetsAtEndOfEachMonth}
+            index="month"
+            categories={["Total assets"]}
+            colors={["orange"]}
+            showAnimation={true}
+            animationDuration={400}
+            curveType="monotone"
+          />
+        </Card>
+      )}
+    </ClientOnly>
   );
 }
