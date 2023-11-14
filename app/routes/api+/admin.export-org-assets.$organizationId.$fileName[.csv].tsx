7098d5fb2a4ea37e9a499d00a9391be6799c9e11
Donkoko <nbonev@duck.com>
2023-11-14 19:13:42 +0200

    adding export button and import form to organization view for admin

--- app/routes/api+/admin.export-org-assets.$organizationId.$fileName[.csv].tsx
+++ app/routes/api+/admin.export-org-assets.$organizationId.$fileName[.csv].tsx
@@ -1,12 +1,14 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { requireAuthSession } from "~/modules/auth";
-import { requireOrganisationId } from "~/modules/organization/context.server";
 import { assertUserCanExportAssets } from "~/modules/tier";
+import { getRequiredParam } from "~/utils";
 import { exportAssetsToCsv } from "~/utils/csv.server";
+import { requireAdmin } from "~/utils/roles.server";
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const authSession = await requireAuthSession(request);
-  const { organizationId } = await requireOrganisationId(authSession, request);
+  await requireAdmin(request);
+  const organizationId = getRequiredParam(params, "organizationId");
   const { userId } = authSession;
 
   await assertUserCanExportAssets({ userId });
