2dd019fa3348dda64197ef2a8d75ce7d74fe8720
hunar <hunar@netling.digital>
2023-11-22 14:39:07 +0530

    configured fallbacks for charts in dashboard

--- app/components/dashboard/inventory-value-chart.tsx
+++ app/components/dashboard/inventory-value-chart.tsx
@@ -4,6 +4,7 @@ import { ClientOnly } from "remix-utils/client-only";
 import type { loader } from "~/routes/_layout+/dashboard";
 import { EmptyState } from "./empty-state";
 import { InfoTooltip } from "../shared/info-tooltip";
+import FallbackLoading from "./fallback-loading";
 
 export default function InventoryValueChart() {
   const { assets, currency, totalAssets, totalValuation, locale } =
@@ -13,32 +14,34 @@ export default function InventoryValueChart() {
   ).length;
 
   return (
-    <ClientOnly fallback={null}>
-      {() => (
-        <div className="w-full border border-gray-200 ">
-          <div className="flex items-center justify-between">
-            <div className="flex-1 border-b p-4 text-left text-[14px] font-semibold  text-gray-900 md:px-6">
-              Inventory value
-            </div>
-            <div className="border-b p-4 text-right text-[14px] font-semibold  text-gray-900 md:px-6">
-              <InfoTooltip
-                content={
-                  <>
-                    <h6>Inventory value</h6>
-                    <p>This chart shows how much value your assets hold</p>
-                  </>
-                }
-              />
-            </div>
-          </div>
-          <div className="h-full p-8">
-            {valueKnownAssets > 0 ? (
-              <div className="space-y-3">
-                <Flex
-                  className="space-x-5"
-                  justifyContent="evenly"
-                  alignItems="end"
-                >
+    <div className="w-full border border-gray-200 ">
+      <div className="flex items-center justify-between">
+        <div className="flex-1 border-b p-4 text-left text-[14px] font-semibold  text-gray-900 md:px-6">
+          Inventory value
+        </div>
+        <div className="border-b p-4 text-right text-[14px] font-semibold  text-gray-900 md:px-6">
+          <InfoTooltip
+            content={
+              <>
+                <h6>Inventory value</h6>
+                <p>This chart shows how much value your assets hold</p>
+              </>
+            }
+          />
+        </div>
+      </div>
+      <div className="h-full p-8">
+        {valueKnownAssets > 0 ? (
+          <div className="space-y-3">
+            <Flex
+              className="space-x-5"
+              justifyContent="evenly"
+              alignItems="end"
+            >
+              <ClientOnly
+                fallback={<FallbackLoading className="h-[150px] w-[150px]" />}
+              >
+                {() => (
                   <ProgressCircle
                     value={(valueKnownAssets / totalAssets) * 100}
                     size="xl"
@@ -54,26 +57,26 @@ export default function InventoryValueChart() {
                       )}%)`}
                     </span>
                   </ProgressCircle>
-                  <div>
-                    <Text className="mb-2 !text-[14px] font-medium text-gray-600">
-                      Inventory value
-                    </Text>
-                    <Text className="mb-3 !text-[30px] font-semibold text-gray-900">
-                      {currency}{" "}
-                      {(totalValuation || 0).toLocaleString(locale, {
-                        style: "currency",
-                        currency: currency,
-                      })}
-                    </Text>
-                  </div>
-                </Flex>
+                )}
+              </ClientOnly>
+              <div>
+                <Text className="mb-2 !text-[14px] font-medium text-gray-600">
+                  Inventory value
+                </Text>
+                <Text className="mb-3 !text-[30px] font-semibold text-gray-900">
+                  {currency}{" "}
+                  {(totalValuation || 0).toLocaleString(locale, {
+                    style: "currency",
+                    currency: currency,
+                  })}
+                </Text>
               </div>
-            ) : (
-              <EmptyState text="No assets with values exists in database" />
-            )}
+            </Flex>
           </div>
-        </div>
-      )}
-    </ClientOnly>
+        ) : (
+          <EmptyState text="No assets with values exists in database" />
+        )}
+      </div>
+    </div>
   );
 }
