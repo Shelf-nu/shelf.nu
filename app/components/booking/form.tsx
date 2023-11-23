40249f29891dfc76224c6d38a23b0e9e2ff25690
hunar <hunar@netling.digital>
2023-11-23 20:18:42 +0530

    UI updates

--- app/components/booking/form.tsx
+++ app/components/booking/form.tsx
@@ -1,5 +1,16 @@
-import type { ZodType } from "zod";
+import { Form, useNavigation } from "@remix-run/react";
+import { useAtom } from "jotai";
+import { useZorm } from "react-zorm";
 import { z } from "zod";
+import { updateDynamicTitleAtom } from "~/atoms/dynamic-title-atom";
+import { isFormProcessing } from "~/utils/form";
+import { zodFieldIsRequired } from "~/utils/zod";
+import CustodianSelect from "../custody/custodian-select";
+import FormRow from "../forms/form-row";
+import Input from "../forms/input";
+import { Button } from "../shared/button";
+import { Card } from "../shared/card";
+import { Spinner } from "../shared/spinner";
 
 type FormData = {
   name?: string;
@@ -9,7 +20,7 @@ type FormData = {
 };
 
 //z.coerce.date() is used to convert the string to a date object.
-export const BookingFormSchema: ZodType<FormData> = z
+export const BookingFormSchema = z
   .object({
     name: z.string().min(2, "Name is required"),
     startDate: z.coerce.date().refine((data) => data > new Date(), {
@@ -29,5 +40,103 @@ export function BookingForm({
   endDate,
   custodianId,
 }: FormData) {
-  return <div>BookingForm</div>;
+  const navigation = useNavigation();
+  const zo = useZorm("NewQuestionWizardScreen", BookingFormSchema);
+  const disabled = isFormProcessing(navigation.state);
+
+  const [, updateName] = useAtom(updateDynamicTitleAtom);
+  return (
+    <div className="flex items-center gap-4">
+      <div className="w-[328px]"></div>
+      <div className="flex-1">
+        <Form
+          ref={zo.ref}
+          method="post"
+          className="flex w-full flex-col gap-2"
+          encType="multipart/form-data"
+        >
+          <Card>
+            <FormRow
+              rowLabel={"Name"}
+              className="border-b-0 pb-[10px]"
+              required={zodFieldIsRequired(BookingFormSchema.shape.name)}
+            >
+              <Input
+                label="Name"
+                hideLabel
+                name={zo.fields.name()}
+                disabled={disabled}
+                error={zo.errors.name()?.message}
+                autoFocus
+                onChange={updateName}
+                className="w-full"
+                defaultValue={name || undefined}
+                placeholder="Booking"
+                required={zodFieldIsRequired(BookingFormSchema.shape.name)}
+              />
+            </FormRow>
+          </Card>
+          <Card>
+            <FormRow
+              rowLabel={"Start Date"}
+              className="border-b-0 pb-[10px]"
+              required={zodFieldIsRequired(BookingFormSchema.shape.startDate)}
+            >
+              <Input
+                label="Start Date"
+                type="datetime-local"
+                hideLabel
+                name={zo.fields.startDate()}
+                disabled={disabled}
+                error={zo.errors.startDate()?.message}
+                autoFocus
+                onChange={updateName}
+                className="w-full"
+                defaultValue={startDate || undefined}
+                placeholder="Booking"
+                required={zodFieldIsRequired(BookingFormSchema.shape.startDate)}
+              />
+            </FormRow>
+            <FormRow
+              rowLabel={"End Date"}
+              className="border-b-0 pb-[10px]"
+              required={zodFieldIsRequired(BookingFormSchema.shape.endDate)}
+            >
+              <Input
+                label="End Date"
+                type="datetime-local"
+                hideLabel
+                name={zo.fields.endDate()}
+                disabled={disabled}
+                error={zo.errors.endDate()?.message}
+                autoFocus
+                onChange={updateName}
+                className="w-full"
+                defaultValue={endDate || undefined}
+                placeholder="Booking"
+                required={zodFieldIsRequired(BookingFormSchema.shape.endDate)}
+              />
+            </FormRow>
+            <p className="text-[14px] text-gray-600">
+              Within this period the assets in this booking will be in custody
+              and unavailable for other bookings.
+            </p>
+          </Card>
+          <Card>
+            <CustodianSelect />
+            <p className="text-[14px] text-gray-600">
+              The person that will be in custody of or responsible for the
+              assets during the duration of the booking period.
+            </p>
+          </Card>
+
+          <div className="text-right">
+            <Button type="submit" disabled={disabled}>
+              {disabled ? <Spinner /> : "Save"}
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
--- app/routes/_layout+/bookings._index.tsx
+++ app/routes/_layout+/bookings._index.tsx
@@ -1,9 +1,13 @@
 import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
+import { ChevronRight } from "~/components/icons";
 import Header from "~/components/layout/header";
 import type { HeaderData } from "~/components/layout/header/types";
-import { Button } from "~/components/shared";
+import { List } from "~/components/list";
+import { Filters } from "~/components/list/filters";
+import { Badge, Button } from "~/components/shared";
+import { Td, Th } from "~/components/table";
 import { requireAuthSession } from "~/modules/auth";
 import { requireOrganisationId } from "~/modules/organization/context.server";
 import {
@@ -17,6 +21,73 @@ import {
   updateCookieWithPerPage,
   userPrefs,
 } from "~/utils/cookies.server";
+
+const bookings = [
+  {
+    id: 1,
+    name: "Untitled booking",
+    status: "DRAFT",
+    from: null,
+    to: null,
+    custodian: null,
+  },
+  {
+    id: 2,
+    name: "Bit Summit 2024",
+    status: "DRAFT",
+    from: { date: "12 Jul 2024", day: "Tue", time: "10:00" },
+    to: { date: "14 Jul 2024", day: "Fri", time: "20:00" },
+    custodian: "Olivia Rhye",
+  },
+  {
+    id: 3,
+    name: "Random convention 2023",
+    status: "ACTIVE",
+    from: { date: "12 Jul 2024", day: "Tue", time: "10:00" },
+    to: { date: "14 Jul 2024", day: "Fri", time: "20:00" },
+    custodian: "Olivia Rhye",
+  },
+  {
+    id: 4,
+    name: "Bit Summit 2023",
+    status: "ACTIVE",
+    from: { date: "12 Jul 2024", day: "Tue", time: "10:00" },
+    to: { date: "14 Jul 2024", day: "Fri", time: "20:00" },
+    custodian: "Olivia Rhye",
+  },
+  {
+    id: 5,
+    name: "Random convention 2023",
+    status: "COMPLETED",
+    from: { date: "12 Jul 2024", day: "Tue", time: "10:00" },
+    to: { date: "14 Jul 2024", day: "Fri", time: "20:00" },
+    custodian: "Olivia Rhye",
+  },
+  {
+    id: 6,
+    name: "Random convention 2022",
+    status: "COMPLETED",
+    from: { date: "12 Jul 2024", day: "Tue", time: "10:00" },
+    to: { date: "14 Jul 2024", day: "Fri", time: "20:00" },
+    custodian: "Olivia Rhye",
+  },
+  {
+    id: 7,
+    name: "Random convention 2021",
+    status: "COMPLETED",
+    from: { date: "12 Jul 2024", day: "Tue", time: "10:00" },
+    to: { date: "14 Jul 2024", day: "Fri", time: "20:00" },
+    custodian: "Olivia Rhye",
+  },
+  {
+    id: 8,
+    name: "Random convention 2020",
+    status: "COMPLETED",
+    from: { date: "12 Jul 2024", day: "Tue", time: "10:00" },
+    to: { date: "14 Jul 2024", day: "Fri", time: "20:00" },
+    custodian: "Olivia Rhye",
+  },
+];
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const authSession = await requireAuthSession(request);
@@ -46,11 +117,11 @@ export async function loader({ request }: LoaderFunctionArgs) {
   return json(
     {
       header,
-      // items: locations,
+      items: bookings,
       search,
       page,
-      // totalItems: totalLocations,
-      // totalPages,
+      totalItems: bookings.length,
+      totalPages: 1,
       perPage,
       prev,
       next,
@@ -81,17 +152,96 @@ export default function BookingsIndexPage() {
         </Button>
       </Header>
       <div className="mt-8 flex flex-1 flex-col md:mx-0 md:gap-2">
-        List bookings
-        {/* <List
-          ItemComponent={ListItemContent}
+        <List
+          ItemComponent={ListAssetContent}
           navigate={(itemId) => navigate(itemId)}
+          className=" overflow-x-visible md:overflow-x-auto"
           headerChildren={
             <>
-              <Th>Assets</Th>
+              <Th className="hidden md:table-cell">From</Th>
+              <Th className="hidden md:table-cell">To</Th>
+              <Th className="hidden md:table-cell">Custodian</Th>
             </>
           }
-        /> */}
+        />
       </div>
     </>
   );
 }
+
+const ListAssetContent = ({ item }: { item: { [key: string]: any } }) => {
+  const statusColorMap: { [key: string]: string } = {
+    DRAFT: "#667085",
+    ACTIVE: "#7A5AF8",
+    COMPLETED: "#17B26A",
+  };
+  return (
+    <>
+      {/* Item */}
+      <Td className="w-full whitespace-normal p-0 md:p-0">
+        <div className="flex justify-between gap-3 p-4 md:justify-normal md:px-6">
+          <div className="flex items-center gap-3">
+            <div className="min-w-[130px]">
+              <span className="word-break mb-1 block font-medium">
+                {item.name}
+              </span>
+              <div className="">
+                <Badge color={statusColorMap[item.status]}>
+                  <span className="block lowercase first-letter:uppercase">
+                    {item.status}
+                  </span>
+                </Badge>
+              </div>
+            </div>
+          </div>
+
+          <button className="block md:hidden">
+            <ChevronRight />
+          </button>
+        </div>
+      </Td>
+      {/* From */}
+      <Td className="hidden md:table-cell">
+        {item.from ? (
+          <div className="min-w-[130px]">
+            <span className="word-break mb-1 block font-medium">
+              {item.from.date}
+            </span>
+            <span className="block text-gray-600">
+              {item.from.day} {item.from.time}
+            </span>
+          </div>
+        ) : null}
+      </Td>
+
+      {/* To */}
+      <Td className="hidden md:table-cell">
+        {item.to ? (
+          <div className="min-w-[130px]">
+            <span className="word-break mb-1 block font-medium">
+              {item.to.date}
+            </span>
+            <span className="block text-gray-600">
+              {item.to.day} {item.to.time}
+            </span>
+          </div>
+        ) : null}
+      </Td>
+
+      {/* Custodian */}
+      <Td className="hidden md:table-cell">
+        {item.custodian ? (
+          <span className="inline-flex w-max items-center justify-center rounded-2xl bg-gray-100 px-2 py-[2px] text-center text-[12px] font-medium text-gray-700">
+            <img
+              src={"/images/default_pfp.jpg"}
+              className="mr-1 h-4 w-4 rounded-full"
+              alt=""
+            />
+
+            <span className="mt-[1px]">{item.custodian}</span>
+          </span>
+        ) : null}
+      </Td>
+    </>
+  );
+};
