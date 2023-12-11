2c1a6c245613932473da3f81af09e70c69f810a2
Donkoko <nbonev@duck.com>
2023-12-11 12:23:06 +0200

    mend

--- app/hooks/use-formbricks.ts
+++ app/hooks/use-formbricks.ts
@@ -6,7 +6,7 @@ import { FORMBRICKS_ENV_ID, NODE_ENV } from "~/utils";
 
 export function useFormbricks() {
   useEffect(() => {
-    if (window && FORMBRICKS_ENV_ID) {
+    if (typeof window !== "undefined" && FORMBRICKS_ENV_ID) {
       formbricks.init({
         environmentId: FORMBRICKS_ENV_ID,
         apiHost: "https://app.formbricks.com",
