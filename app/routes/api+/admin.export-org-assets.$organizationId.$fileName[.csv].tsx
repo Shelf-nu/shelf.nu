941bacba3be812e22257ef853b6bc0459d6115de
Donkoko <nbonev@duck.com>
2024-01-18 12:43:52 +0200

    removed assertUserCanExportAssets from the admin export endpoint

--- app/routes/api+/admin.export-org-assets.$organizationId.$fileName[.csv].tsx
+++ app/routes/api+/admin.export-org-assets.$organizationId.$fileName[.csv].tsx
@@ -1,18 +1,17 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { requireAuthSession } from "~/modules/auth";
-import { requireOrganisationId } from "~/modules/organization/context.server";
-import { assertUserCanExportAssets } from "~/modules/tier";
 import { getRequiredParam } from "~/utils";
 import { exportAssetsToCsv } from "~/utils/csv.server";
 import { requireAdmin } from "~/utils/roles.server";
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
-  const authSession = await requireAuthSession(request);
+  await requireAuthSession(request);
   await requireAdmin(request);
   const organizationId = getRequiredParam(params, "organizationId");
-  const { organizations } = await requireOrganisationId(authSession, request);
+  // const { organizations } = await requireOrganisationId(authSession, request);
 
-  await assertUserCanExportAssets({ organizationId, organizations });
+  /** We dont need to do this anymore, because this is only for admin */
+  // await assertUserCanExportAssets({ organizationId, organizations });
 
   /** Join the rows with a new line */
   const csvString = await exportAssetsToCsv({ organizationId });
