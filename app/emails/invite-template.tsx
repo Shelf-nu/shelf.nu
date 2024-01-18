35b68a12f62e9b9416a1533c5435f60f7070a446
Donkoko <nbonev@duck.com>
2024-01-18 14:22:18 +0200

    testing inline styles instead of tailwind

--- app/emails/invite-template.tsx
+++ app/emails/invite-template.tsx
@@ -1,7 +1,6 @@
 import {
   Button,
   Html,
-  Tailwind,
   Text,
   Img,
   Section,
@@ -9,9 +8,9 @@ import {
   Head,
   render,
 } from "@react-email/components";
-import tailwindConfig from "tailwind.config";
 import type { InviteWithInviterAndOrg } from "~/modules/invite/types";
 import { SERVER_URL } from "~/utils/env";
+import { styles } from "./styles";
 
 interface Props {
   invite: InviteWithInviterAndOrg;
@@ -24,61 +23,69 @@ export function InvitationEmailTemplate({ invite, token }: Props) {
       <Head>
         <title>Invitation to join Shelf as an Administrator</title>
       </Head>
-      <Tailwind config={tailwindConfig}>
-        <Section className="p-14">
-          <Img
-            src="cid:shelf-logo"
-            alt="Shelf's logo"
-            width="100"
-            height="32"
-            className="mb-6"
-          />
-          <div className="py-8">
-            <Text className="mb-6 text-[16px] text-gray-700">
-              Howdy,
-              <br />
-              {invite.inviter.firstName} {invite.inviter.lastName} invites you
-              to join Shelf as an Administrator for {invite.organization.name}
-              ’s workspace. Click the link to accept the invite:
-            </Text>
-            <Button
-              href={`${SERVER_URL}/accept-invite/${invite.id}?token=${token}`}
-              className="box-shadow-xs mb-6 inline-flex max-w-xl items-center justify-center gap-2 rounded border border-primary-400 bg-primary-500 px-4 py-[10px] text-center text-[14px] font-semibold text-white focus:ring-2 hover:bg-primary-400"
+
+      <Section style={{ padding: "14px" }}>
+        <Img
+          src="cid:shelf-logo"
+          alt="Shelf's logo"
+          width="100"
+          height="32"
+          style={{ marginBottom: "1.5rem" }}
+        />
+        <div style={{ paddingTop: "8px" }}>
+          <Text
+            style={{ marginBottom: "6px", fontSize: "16px", color: "#4B5563" }}
+          >
+            Howdy,
+            <br />
+            {invite.inviter.firstName} {invite.inviter.lastName} invites you to
+            join Shelf as an Administrator for {invite.organization.name}
+            ’s workspace. Click the link to accept the invite:
+          </Text>
+          <Button
+            href={`${SERVER_URL}/accept-invite/${invite.id}?token=${token}`}
+            style={{ ...styles.button, textAlign: "center" }}
+          >
+            Accept the invite
+          </Button>
+          <Text
+            style={{ marginBottom: "6px", fontSize: "16px", color: "#4B5563" }}
+          >
+            Once you’re done setting up your account, you'll be able to access
+            the workspace and start exploring features like Asset Explorer,
+            Location Tracking, Collaboration, Custom fields and more. If you
+            have any questions or need assistance, please don't hesitate to
+            contact our support team at{" "}
+            <Link
+              style={{ color: "primary-700" }}
+              href="mailto:support@shelf.nu"
             >
-              Accept the invite
-            </Button>
-            <Text className="mb-6 text-[16px] text-gray-700">
-              Once you’re done setting up your account, you'll be able to access
-              the workspace and start exploring features like Asset Explorer,
-              Location Tracking, Collaboration, Custom fields and more. If you
-              have any questions or need assistance, please don't hesitate to
-              contact our support team at{" "}
-              <Link className="text-primary-700" href="mailto:support@shelf.nu">
-                support@shelf.nu
-              </Link>
-              .
-            </Text>
-            <Text className="mb-8 text-[16px] text-gray-700">
-              Thanks, <br />
-              The Shelf team
-            </Text>
-            <Text className="text-[14px] text-gray-700">
-              This is an automatic email sent from{" "}
-              <Link className="text-primary-700" href="https://www.shelf.nu/">
-                shelf.nu
-              </Link>{" "}
-              to{" "}
-              <Link
-                className="text-primary-700"
-                href={`mailto:${invite.inviteeEmail}`}
-              >
-                {invite.inviteeEmail}
-              </Link>
-              .
-            </Text>
-          </div>
-        </Section>
-      </Tailwind>
+              support@shelf.nu
+            </Link>
+            .
+          </Text>
+          <Text
+            style={{ marginBottom: "8px", fontSize: "16px", color: "#4B5563" }}
+          >
+            Thanks, <br />
+            The Shelf team
+          </Text>
+          <Text style={{ fontSize: "14px", color: "#4B5563" }}>
+            This is an automatic email sent from{" "}
+            <Link style={{ color: "primary-700" }} href="https://www.shelf.nu/">
+              shelf.nu
+            </Link>{" "}
+            to{" "}
+            <Link
+              style={{ color: "primary-700" }}
+              href={`mailto:${invite.inviteeEmail}`}
+            >
+              {invite.inviteeEmail}
+            </Link>
+            .
+          </Text>
+        </div>
+      </Section>
     </Html>
   );
 }
---
+++ app/emails/styles.ts
@@ -0,0 +1,18 @@
+export const styles = {
+  button: {
+    marginBottom: "6px",
+    display: "inline-flex",
+    maxWidth: "36rem",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "2",
+    borderRadius: "border",
+    border: "rgb(243 135 68)",
+    backgroundColor: "rgb(239 104 32)",
+    padding: "4px",
+    textAlign: "center",
+    fontSize: "14px",
+    fontWeight: "semibold",
+    color: "white",
+  },
+};
