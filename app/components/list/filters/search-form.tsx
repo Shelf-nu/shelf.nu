b6e2f0bdb1bc880900551328afef627dbf9c807a
Donkoko <nbonev@duck.com>
2024-01-26 12:01:26 +0200

    fixing clear search on main form

--- app/components/list/filters/search-form.tsx
+++ app/components/list/filters/search-form.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import {
   useLoaderData,
   useNavigation,
@@ -7,21 +8,34 @@ import {
 import Input from "~/components/forms/input";
 import { Button } from "~/components/shared/button";
 import type { SearchableIndexResponse } from "~/modules/types";
+import { isFormProcessing } from "~/utils";
 import { SearchFieldTooltip } from "./search-field-tooltip";
 
 export const SearchForm = () => {
-  const [params] = useSearchParams();
+  const [_searchParams, setSearchParams] = useSearchParams();
 
   const { search, modelName, searchFieldLabel } =
     useLoaderData<SearchableIndexResponse>();
   const { singular } = modelName;
-  const state = useNavigation().state;
-  const isSearching =
-    state === "loading" && (params.has("s") || params.has("category"));
+  const navigation = useNavigation();
+  const isSearching = isFormProcessing(navigation.state);
+  const submitButtonRef = useRef<HTMLButtonElement>(null);
+  const searchInputRef = useRef<HTMLInputElement>(null);
 
   const label = searchFieldLabel
     ? searchFieldLabel
     : `Search by ${singular} name`;
+
+  function clearSearch() {
+    setSearchParams((prev) => {
+      prev.delete("s");
+
+      return prev;
+    });
+    if (searchInputRef.current) {
+      searchInputRef.current.value = "";
+    }
+  }
 
   return (
     <div className="flex w-full md:w-auto">
@@ -32,12 +46,18 @@ export const SearchForm = () => {
           label={label}
           aria-label={label}
           placeholder={label}
-          defaultValue={search}
+          defaultValue={search || ""}
           disabled={isSearching}
           hideLabel
           hasAttachedButton
           className="w-full md:w-auto"
           inputClassName="pr-9"
+          ref={searchInputRef}
+          onKeyDown={(e) => {
+            if (e.key == "Enter") {
+              submitButtonRef.current?.click();
+            }
+          }}
         />
         {search ? (
           <Button
@@ -49,6 +69,7 @@ export const SearchForm = () => {
             value="clearSearch"
             title="Clear search"
             className="absolute right-3.5 top-1/2 -translate-y-1/2 cursor-pointer border-0 p-0 text-gray-400 hover:text-gray-700"
+            onClick={clearSearch}
           />
         ) : (
           <SearchFieldTooltip />
@@ -61,6 +82,7 @@ export const SearchForm = () => {
         title="Search"
         disabled={isSearching}
         attachToInput
+        ref={submitButtonRef}
       />
     </div>
   );
