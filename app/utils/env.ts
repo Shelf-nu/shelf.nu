a90dad31ee79de68261598d2dfcee3f4be0379c1
Donkoko <nbonev@duck.com>
2024-01-29 15:12:58 +0200

    adjusting SENTRY_DSN, making it not required

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -12,7 +12,6 @@ declare global {
       MICROSOFT_CLARITY_ID: string;
       CRISP_WEBSITE_ID: string;
       ENABLE_PREMIUM_FEATURES: string;
-      FORMBRICKS_ENV_ID: string;
       MAINTENANCE_MODE: string;
     };
   }
@@ -34,7 +33,6 @@ declare global {
       STRIPE_WEBHOOK_ENDPOINT_SECRET: string;
       ENABLE_PREMIUM_FEATURES: string;
       INVITE_TOKEN_SECRET: string;
-      FORMBRICKS_ENV_ID: string;
       SMTP_PWD: string;
       SMTP_HOST: string;
       SMTP_USER: string;
@@ -108,7 +106,9 @@ export const SMTP_HOST = getEnv("SMTP_HOST");
 export const SMTP_USER = getEnv("SMTP_USER");
 export const DATABASE_URL = getEnv("DATABASE_URL");
 export const DIRECT_URL = getEnv("DIRECT_URL");
-export const SENTRY_DSN = getEnv("SENTRY_DSN") || false;
+export const SENTRY_DSN = getEnv("SENTRY_DSN", {
+  isRequired: false,
+});
 
 /**
  * Shared envs
@@ -163,7 +163,6 @@ export function getBrowserEnv() {
     CRISP_WEBSITE_ID,
     MICROSOFT_CLARITY_ID,
     ENABLE_PREMIUM_FEATURES,
-    FORMBRICKS_ENV_ID,
     MAINTENANCE_MODE,
   };
 }
