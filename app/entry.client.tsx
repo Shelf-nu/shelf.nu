d0efe9f511a84efdaa35504f0cc20ad453832b09
Donkoko <nbonev@duck.com>
2024-01-29 15:00:57 +0200

    integrating sentry

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import { RemixBrowser } from "@remix-run/react";
+import { RemixBrowser, useLocation, useMatches } from "@remix-run/react";
+import * as Sentry from "@sentry/remix";
 import { Provider as JotaiProvider } from "jotai";
 import { hydrateRoot } from "react-dom/client";
+import { SENTRY_DSN } from "./utils";
+
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    tracesSampleRate: 1,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+
+    integrations: [
+      new Sentry.BrowserTracing({
+        routingInstrumentation: Sentry.remixRouterInstrumentation(
+          useEffect,
+          useLocation,
+          useMatches
+        ),
+      }),
+      new Sentry.Replay(),
+    ],
+  });
+}
 
 function hydrate() {
   React.startTransition(() => {
