e041d0b7d62fbc9404c55f4ebd0e8916a52f5b32
Donkoko <nbonev@duck.com>
2024-01-30 18:24:23 +0200

    initializing sentry with initial data for user

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -10,6 +10,7 @@ import { RemixServer } from "@remix-run/react";
 import * as Sentry from "@sentry/remix";
 import isbot from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
+import { getAuthSession } from "./modules/auth";
 import { registerBookingWorkers } from "./modules/booking";
 import { NODE_ENV, SENTRY_DSN } from "./utils";
 import * as schedulerService from "./utils/scheduler.server";
@@ -46,6 +47,22 @@ export default async function handleRequest(
   await schedulerService.init();
   registerBookingWorkers();
   // === end: register scheduler and workers ===
+
+  // === Init Sentry
+  const authSession = await getAuthSession(request);
+  if (SENTRY_DSN && NODE_ENV === "production") {
+    Sentry.init({
+      dsn: SENTRY_DSN,
+      tracesSampleRate: 1,
+      environment: NODE_ENV,
+      initialScope: {
+        user: {
+          id: authSession ? authSession.userId : "unknown",
+        },
+      },
+    });
+  }
+  // === END Init Sentry
 
   return new Promise(async (res, reject) => {
     let didError = false;
