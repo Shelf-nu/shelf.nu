65231a2e37a396d2627312a793a2a82b46ee072e
Donkoko <nbonev@duck.com>
2024-02-21 20:24:10 +0200

    mend

--- app/components/shared/tabs.tsx
+++ app/components/shared/tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   return (
     <TabsPrimitive.Trigger
       className={tw(
-        "inline-flex min-w-[100px] items-center justify-center rounded-[0.185rem] px-2.5 py-1  text-text-sm font-medium text-gray-700 transition-all  disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-900 data-[state=active]:shadow-sm ",
+        "inline-flex min-w-[100px] items-center justify-center rounded-[0.185rem] px-2.5 py-1  text-text-sm font-medium text-gray-700 transition-all  data-[state=active]:bg-white data-[state=active]:text-slate-900 data-[state=active]:shadow-sm disabled:pointer-events-none disabled:opacity-50 ",
         className
       )}
       {...props}
