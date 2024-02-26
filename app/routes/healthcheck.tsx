c0393ef1ed480aa97cfe81334a5be7f2ff332150
Donkoko <nbonev@duck.com>
2024-02-26 15:02:21 +0200

    new fly.toml config + adjustments to healthcheck

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,22 +1,13 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
-import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
 
 import { db } from "~/database";
 
-export async function loader({ request }: LoaderFunctionArgs) {
-  const host =
-    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
-
+export async function loader() {
   try {
-    const url = new URL("/", `http://${host}`);
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
-    await Promise.all([
-      db.user.findFirst(),
-      fetch(url.toString(), { method: "HEAD" }).then((r) => {
-        if (!r.ok) return Promise.reject(r);
-      }),
-    ]);
+    await db.user.findFirst();
     return json({ status: "OK" });
   } catch (error: unknown) {
     // eslint-disable-next-line no-console
