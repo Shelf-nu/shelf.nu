52973c7ed3475d99bf161efb22f5f9114e412c18
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-02-28 07:10:30 +0530

    refactor(model-filters): filters models based on organizationId instead of userId

--- app/routes/api+/model-filters.ts
+++ app/routes/api+/model-filters.ts
@@ -2,6 +2,7 @@ import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { z } from "zod";
 import { db } from "~/database";
+import { requireOrganisationId } from "~/modules/organization/context.server";
 
 export type AllowedModelNames = "asset" | "tag" | "category" | "location";
 
@@ -23,6 +24,8 @@ export async function loader({ context, request }: LoaderFunctionArgs) {
   const authSession = context.getSession();
   const { userId } = authSession;
 
+  const { organizationId } = await requireOrganisationId({ userId, request });
+
   /** Getting all the query parameters from url */
   const url = new URL(request.url);
   const data: Record<string, string> = {};
@@ -39,7 +42,7 @@ export async function loader({ context, request }: LoaderFunctionArgs) {
   const model = result.data.model as AllowedModelNames;
   const queryData = (await db[model].dynamicFindMany({
     where: {
-      userId,
+      organizationId,
       OR: [
         {
           [result.data.queryKey]: {
