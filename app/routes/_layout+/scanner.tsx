18b6b6650cc946a30e01d1a8705832a5576e9787
Donkoko <nbonev@duck.com>
2024-03-21 19:09:42 +0200

    handling missing cases, and abstracting functionality in its own hook useQRScanner

--- app/routes/_layout+/scanner.tsx
+++ app/routes/_layout+/scanner.tsx
@@ -1,18 +1,13 @@
-import { useState } from "react";
-import type { MetaFunction } from "@remix-run/node";
-import { useNavigate, Link, json } from "@remix-run/react";
-import { useMediaDevices } from "react-media-devices";
-import { useZxing } from "react-zxing";
+import { json, type MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 import { ErrorContent } from "~/components/errors";
 import Header from "~/components/layout/header";
 import type { HeaderData } from "~/components/layout/header/types";
-import { useClientNotification } from "~/hooks/use-client-notification";
+import { Spinner } from "~/components/shared/spinner";
+import { useQrScanner } from "~/hooks/use-video-devices";
 import { appendToMetaTitle } from "~/utils/append-to-meta-title";
-import type { ErrorLabel } from "~/utils/error";
-import { makeShelfError, ShelfError } from "~/utils/error";
+import { makeShelfError } from "~/utils/error";
 import { error } from "~/utils/http.server";
-
-const label: ErrorLabel = "Scanner";
 
 export function loader() {
   try {
@@ -35,60 +30,14 @@ export const meta: MetaFunction<typeof loader> = () => [
 ];
 
 const QRScanner = () => {
-  const [sendNotification] = useClientNotification();
-  const navigate = useNavigate();
-  const [selectedDevice, setSelectedDevice] = useState("");
-
-  const { devices } = useMediaDevices({
-    constraints: {
-      video: true,
-      audio: false,
-    },
-  });
-  const videoMediaDevices = devices
-    ? devices.filter((device) => device.kind === "videoinput")
-    : [];
-
-  const { ref: videoRef } = useZxing({
-    deviceId: selectedDevice,
-    constraints: { video: true, audio: false },
-    onDecodeResult(result) {
-      decodeQRCodes(result.getText());
-    },
-    onError(cause) {
-      throw new ShelfError({
-        message: "Unable to access media devices permission",
-        status: 403,
-        label,
-        cause,
-      });
-    },
-  });
-
-  const decodeQRCodes = (result: string) => {
-    if (result != null) {
-      const regex = /^(https?:\/\/)([^/:]+)(:\d+)?\/qr\/([a-zA-Z0-9]+)$/;
-      /** We make sure the value of the QR code matches the structure of Shelf qr codes */
-      const match = result.match(regex);
-      if (!match) {
-        /** If the QR code does not match the structure of Shelf qr codes, we show an error message */
-        sendNotification({
-          title: "QR Code Not Valid",
-          message: "Please Scan valid asset QR",
-          icon: { name: "trash", variant: "error" },
-        });
-        return;
-      }
-
-      sendNotification({
-        title: "Shelf's QR Code detected",
-        message: "Redirecting to mapped asset",
-        icon: { name: "success", variant: "success" },
-      });
-      const qrId = match[4]; // Get the last segment of the URL as the QR id
-      navigate(`/qr/${qrId}`);
-    }
-  };
+  const {
+    ref,
+    videoMediaDevices,
+    selectedDevice,
+    setSelectedDevice,
+    hasPermission,
+    noCamera,
+  } = useQrScanner();
 
   return (
     <>
@@ -96,28 +45,40 @@ const QRScanner = () => {
       <div className=" flex h-[calc(100vh-191px)] flex-col md:h-[calc(100vh-156px)]">
         <div className="m-auto h-5/6 min-h-[400px] py-6 xl:w-[70%]">
           <div className="relative h-full">
-            <video
-              ref={videoRef}
-              width="100%"
-              autoPlay={true}
-              controls={false}
-              className={`pointer-events-none h-full object-cover`}
-            />
-            {videoMediaDevices.length > 0 && (
-              <select
-                className="absolute bottom-3 left-3 z-10 w-[calc(100%-24px)] rounded border-0"
-                name="devices"
-                onChange={(e) => {
-                  setSelectedDevice(e.currentTarget.value);
-                }}
-                defaultValue={selectedDevice}
-              >
-                {videoMediaDevices.map((device) => (
-                  <option key={device.deviceId} value={device.deviceId}>
-                    {device.label}
-                  </option>
-                ))}
-              </select>
+            {noCamera ? (
+              <div className="flex flex-col items-center justify-center">
+                Your device doesnt have a camera
+              </div>
+            ) : !hasPermission ? (
+              <div className="flex flex-col items-center justify-center">
+                <Spinner /> Waiting for permission to access camera.
+              </div>
+            ) : (
+              <>
+                <video
+                  ref={ref}
+                  width="100%"
+                  autoPlay={true}
+                  controls={false}
+                  className={`pointer-events-none h-full object-cover`}
+                />
+                {videoMediaDevices && videoMediaDevices?.length > 0 ? (
+                  <select
+                    className="absolute bottom-3 left-3 z-10 w-[calc(100%-24px)] rounded border-0"
+                    name="devices"
+                    onChange={(e) => {
+                      setSelectedDevice(e.currentTarget.value);
+                    }}
+                    defaultValue={selectedDevice}
+                  >
+                    {videoMediaDevices.map((device, index) => (
+                      <option key={device.deviceId} value={device.deviceId}>
+                        {device.label ? device.label : `Camera ${index + 1}`}
+                      </option>
+                    ))}
+                  </select>
+                ) : null}
+              </>
             )}
           </div>
         </div>
