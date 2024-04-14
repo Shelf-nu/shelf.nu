027fe39bf66feae66a18dcd0e5b427bfeabc38a0
Andy Bao <contact@andybao.me>
2024-04-13 22:13:27 -0400

    Treat asset description text as pre-formatted
    Display asset description text with `whitespace-pre-wrap` on asset page.
    
    This allows newlines in the asset description to display properly.

--- app/routes/_layout+/assets.$assetId.tsx
+++ app/routes/_layout+/assets.$assetId.tsx
@@ -298,7 +298,7 @@ export default function AssetDetailsPage() {
           />
           {asset.description ? (
             <Card className="mb-3 mt-0 rounded-t-none border-t-0">
-              <p className=" text-gray-600">{asset.description}</p>
+              <p className="whitespace-pre-wrap text-gray-600">{asset.description}</p>
             </Card>
           ) : null}
           {!isSelfService ? (
