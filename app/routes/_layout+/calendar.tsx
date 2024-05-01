317b60f5d17e4fa972710012704fa41f121b39f2
Bhavya Jain <rishikabhavya@gmail.com>
2024-05-01 12:09:52 +0530

    code refactoring and designs update

--- app/routes/_layout+/calendar.tsx
+++ app/routes/_layout+/calendar.tsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import FullCalendar from "@fullcalendar/react";
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import { appendToMetaTitle } from "~/utils/append-to-meta-title";
 import { json } from "@remix-run/node";
-import { useLoaderData, useNavigate } from "@remix-run/react";
+import { useLoaderData, useNavigate, useSearchParams } from "@remix-run/react";
+import { makeShelfError } from "~/utils/error";
+import { Link } from "@remix-run/react";
+import { error } from "~/utils/http.server";
+import Header from "~/components/layout/header";
 const dummyOrganizationId = "shelf";
 const dummyUserId = "shelfnu";
 
@@ -12,68 +17,77 @@ const dummyBookings = [
   {
     id: "booking-1",
     name: "January Booking 1",
-    from: new Date("2024-01-05"),
+    from: new Date("2024-01-05T08:00:00"), // Including time
     to: new Date("2024-01-10"),
     status: "CONFIRMED",
     creatorId: "user-1",
+    custodian: "John Doe", // Adding custodian name
   },
   {
     id: "booking-2",
     name: "January Booking 2",
-    from: new Date("2024-01-15"),
+    from: new Date("2024-01-15T09:30:00"), // Example starting time
     to: new Date("2024-01-20"),
     status: "DRAFT",
     creatorId: "user-2",
+    custodian: "Jane Smith", // Adding custodian name
   },
   {
     id: "booking-3",
     name: "February Booking 1",
-    from: new Date("2024-02-05"),
+    from: new Date("2024-02-05T10:00:00"),
     to: new Date("2024-02-10"),
     status: "CONFIRMED",
     creatorId: "user-3",
+    custodian: "Michael Johnson",
   },
   {
     id: "booking-4",
     name: "February Booking 2",
-    from: new Date("2024-02-15"),
+    from: new Date("2024-02-15T11:00:00"),
     to: new Date("2024-02-20"),
     status: "CANCELLED",
     creatorId: "user-4",
+    custodian: "Emily Brown",
   },
   {
     id: "booking-5",
     name: "March Booking 1",
-    from: new Date("2024-03-05"),
+    from: new Date("2024-03-05T12:00:00"),
     to: new Date("2024-03-10"),
     status: "CONFIRMED",
     creatorId: "user-5",
+    custodian: "Daniel White",
   },
   {
     id: "booking-6",
     name: "March Booking 2",
-    from: new Date("2024-03-15"),
+    from: new Date("2024-03-15T13:00:00"),
     to: new Date("2024-03-20"),
     status: "CONFIRMED",
     creatorId: "user-6",
+    custodian: "Sophia Lee",
   },
   {
-    id: "booking-5",
+    id: "booking-7",
     name: "April Booking 1",
-    from: new Date("2024-04-05"),
+    from: new Date("2024-04-05T14:00:00"),
     to: new Date("2024-04-10"),
     status: "CONFIRMED",
     creatorId: "user-6",
+    custodian: "Olivia Taylor",
   },
   {
-    id: "booking-6",
+    id: "booking-8",
     name: "April Booking 2",
-    from: new Date("2024-04-15"),
+    from: new Date("2024-04-15T15:00:00"),
     to: new Date("2024-04-20"),
     status: "CONFIRMED",
     creatorId: "user-7",
+    custodian: "William Davis",
   },
 ];
+
 
 // As per the DB Model
 const dummyBookingsResponse = {
@@ -86,77 +100,99 @@ const dummyBookingsResponse = {
   userId: dummyUserId,
 };
 
-// Loader Function to Return Bookings Data
-export const loader = ({ request }: LoaderFunctionArgs) => {
-  const url = new URL(request.url);
-  const monthParam = url.searchParams.get("month");
-  const yearParam = url.searchParams.get("year");
-
-  const currentMonth = monthParam
-    ? parseInt(monthParam, 10) - 1
-    : new Date().getMonth() + 1;
-
-  const currentYear = yearParam
-    ? parseInt(yearParam, 10)
-    : new Date().getFullYear();
-
-  // Filter the bookings for the selected month and year
-  const filteredBookings = dummyBookings.filter(
-    (booking) =>
-      booking.from.getMonth() === currentMonth &&
-      booking.from.getFullYear() === currentYear
-  );
-
-  const header = {
-    title: `Calendar for ${currentMonth + 1}/${currentYear}`,
-  };
-
-  return json({ bookings: filteredBookings, header });
+export const handle = {
+  breadcrumb: () => <Link to="/calendar">Calendar</Link>,
 };
 
-interface CalendarEvent {
-  title: string;
-  start: string;
-  end: string;
-}
+const formatTime = (date:any) => {
+  return date.toLocaleTimeString("en-US", { 
+    hour: 'numeric', 
+    minute: 'numeric', 
+    hour12: true,
+    meridiem: false 
+  });
+};
+
+// Loader Function to Return Bookings Data
+export const loader = ({ request }: LoaderFunctionArgs) => {
+  try{
+    const url = new URL(request.url);
+    const monthParam = url.searchParams.get("month");
+    const yearParam = url.searchParams.get("year");
+  
+    const currentMonth = monthParam
+      ? parseInt(monthParam, 10) - 1
+      : new Date().getMonth() + 1;
+    
+    const currentYear = yearParam
+      ? parseInt(yearParam, 10)
+      : new Date().getFullYear();
+  
+    const calendarEvents = dummyBookings
+      .filter(
+        (booking) =>
+          booking.from.getMonth() === currentMonth &&
+          booking.from.getFullYear() === currentYear
+      )
+      .map((booking) => ({
+        title: `${formatTime(booking.from)} | ${booking.name} | ${booking.custodian}`,
+      start: booking.from.toISOString(),
+      end: booking.to.toISOString(),
+      }));
+  
+    const header = {
+      title: `Calendar`,
+    };
+  
+    return json({ header, calendarEvents });
+  }
+  catch(cause){
+    const reason = makeShelfError(cause);
+    throw json(error(reason), { status: reason.status });
+  }
+};
+export const meta: MetaFunction<typeof loader> = ({ data }) => [
+  { title: appendToMetaTitle(data?.header.title) },
+];
+
 // Calendar Component
 const Calendar = () => {
-  const { bookings, header } = useLoaderData<typeof loader>();
-  const navigate = useNavigate();
-  const [calendarEvents, setCalendarEvents] = useState<CalendarEvent[]>([]);
-  useEffect(() => {
-    if (bookings && bookings.length > 0) {
-      const events = bookings.map((booking) => ({
-        title: booking.name,
-        start: booking.from,
-        end: booking.to,
-      }));
-      setCalendarEvents(events); // Set the new events
-    }
-  }, [bookings]);
+  const { header, calendarEvents } = useLoaderData<typeof loader>();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const handleMonthChange = (info: any) => {
     const newMonth = !(info.start.getDate() == 1)
       ? info.start.getMonth() + 1
       : info.start.getMonth();
     const newYear = info.start.getFullYear();
-    navigate(`?month=${newMonth + 1}&year=${newYear}`);
+    setSearchParams({
+      month:(newMonth+1).toString(),
+      year: newYear.toString()
+    })
   };
 
   return (
     <>
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        firstDay={1}
-        initialView="dayGridMonth"
-        timeZone="local"
-        events={calendarEvents}
-        datesSet={handleMonthChange}
-      />
+      <Header hidePageDescription={true}/>
+      <div className="mt-4">
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          firstDay={1}
+          headerToolbar={
+            {
+              start: 'title',
+              center: '',
+              end: 'prev today next'
+            }
+          }
+          initialView="dayGridMonth"
+          timeZone="local"
+          events={calendarEvents}
+          datesSet={handleMonthChange}
+        />
+      </div>
     </>
   );
 };
 
-const calendar = () => <Calendar />;
-
-export default calendar;
+export default Calendar;
