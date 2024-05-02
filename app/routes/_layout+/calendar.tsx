ffc01e3dcbd636a5b3d1e3989e8b8bf58dae150e
Bhavya Jain <rishikabhavya@gmail.com>
2024-05-02 11:47:35 +0530

    adding custom header component

--- app/routes/_layout+/calendar.tsx
+++ app/routes/_layout+/calendar.tsx
@@ -1,9 +1,12 @@
+import { useRef, useState } from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import FullCalendar from "@fullcalendar/react";
 import { OrganizationRoles } from "@prisma/client";
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData, useSearchParams, Link } from "@remix-run/react";
+import { format } from "date-fns";
 import Header from "~/components/layout/header";
 import { getBookingsForCalendar } from "~/modules/booking/service.server";
 import calendarStyles from "~/styles/layout/calendar.css?url";
@@ -16,9 +19,6 @@ import {
   PermissionEntity,
 } from "~/utils/permissions/permission.validator.server";
 import { requirePermission } from "~/utils/roles.server";
-import { useRef, useState } from "react";
-import {format} from "date-fns";
-import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 
 export function links() {
   return [{ rel: "stylesheet", href: calendarStyles }];
@@ -75,7 +75,7 @@ const Calendar = () => {
       : info.start.getMonth();
     const newYear = info.start.getFullYear();
     const date = new Date(newYear, newMonth);
-    setTitle(`${format(date, "MMMM")} ${newYear}`)
+    setTitle(`${format(date, "MMMM")} ${newYear}`);
     setSearchParams({
       month: (newMonth + 1).toString(),
       year: newYear.toString(),
@@ -92,22 +92,34 @@ const Calendar = () => {
   const handlePrev = () => {
     const calendarApi = calendarRef.current?.getApi();
     console.log(calendarRef.current);
-    
+
     calendarApi?.prev();
   };
-  const handleGoToCurrentDate = () => {
-
-  }
+  const handleGoToCurrentDate = () => {};
   return (
     <>
       <Header hidePageDescription={true} />
       <div className="mt-4">
-        <div className="border border-[#EAECF0] px-4 py-3 gap-4 flex justify-between items-center rounded-t-md">
-          <div className="text-[#101828] font-sans text-lg font-semibold leading-[20px] text-left">{title}</div>
-          <div className="flex border border-[#D0D5DD] rounded gap-0 shadow-sm">
-            <button className="border-r border-[#D0D5DD] p-2 gap-2 text-[#667085]" onClick={handlePrev}><ChevronLeftIcon/></button>
-            <div className="border-r border-[#D0D5DD] px-3 py-2 text-[#344054] font-sans text-sm font-semibold leading-[20px] text-left cursor-pointer" onClick={handleGoToCurrentDate}>Today</div>
-            <button className="p-2 gap-2 text-[#667085]" onClick={handleNext}><ChevronRightIcon/></button>
+        <div className="flex items-center justify-between gap-4 rounded-t-md border border-DEFAULT px-4 py-3">
+          <div className="text-left font-sans text-lg font-semibold leading-[20px] text-[#101828]">
+            {title}
+          </div>
+          <div className="flex gap-0 rounded border border-[#D0D5DD] shadow-sm">
+            <button
+              className="gap-2 border-r border-[#D0D5DD] p-2 text-[#667085]"
+              onClick={handlePrev}
+            >
+              <ChevronLeftIcon />
+            </button>
+            <div
+              className="cursor-pointer border-r border-[#D0D5DD] px-3 py-2 text-left font-sans text-sm font-semibold leading-[20px] text-[#344054]"
+              onClick={handleGoToCurrentDate}
+            >
+              Today
+            </div>
+            <button className="gap-2 p-2 text-[#667085]" onClick={handleNext}>
+              <ChevronRightIcon />
+            </button>
           </div>
         </div>
         <FullCalendar
