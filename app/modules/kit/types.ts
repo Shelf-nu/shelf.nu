ccc1d65997b6569c5440025ebaebe5aa9885b497
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-05-04 09:32:35 +0530

    feat(kits): enable rls on kit and kit custody tables

--- app/modules/kit/types.ts
+++ app/modules/kit/types.ts
@@ -1,7 +1,15 @@
 import type { Kit } from "@prisma/client";
 
 export type UpdateKitPayload = Partial<
-  Pick<Kit, "name" | "description" | "status" | "image" | "imageExpiration" | "createdById">
+  Pick<
+    Kit,
+    | "name"
+    | "description"
+    | "status"
+    | "image"
+    | "imageExpiration"
+    | "createdById"
+  >
 > & {
   id: Kit["id"];
 };
