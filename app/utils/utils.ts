70c2d6b89857384eef8ee0836040eb09d7d2a798
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-05-27 22:12:05 +0530

    feat(kit-bookings): add doc comment for groupBy

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,3 +1,11 @@
+/**
+ * This method creates an object composed of keys generated from the results
+ * of running each element of collection through the iterate function.
+ *
+ * @param array An array of items
+ * @param by An iterate function which returns a key by which we have to group the array
+ * @returns A single object composed of keys generated using iterate function.
+ */
 export function groupBy<T>(
   array: T[],
   by: (item: T) => string
