778d66e933f201af907cb21230689497c117ba5c
Bhavya Jain <rishikabhavya@gmail.com>
2024-05-31 18:51:01 +0530

    Adding N progress Bar

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import type { PropsWithChildren } from "react";
+import { useEffect, useMemo } from "react";
 import type { User } from "@prisma/client";
 import type {
   LinksFunction,
@@ -15,27 +16,25 @@ import {
   ScrollRestoration,
   useFetchers,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 import { withSentry } from "@sentry/remix";
 
+import NProgress from "nprogress";
+import nProgressStyles from "nprogress/nprogress.css?url";
 import { ErrorContent } from "./components/errors";
 import { HomeIcon } from "./components/icons/library";
 import MaintenanceMode from "./components/layout/maintenance-mode";
 import { Clarity } from "./components/marketing/clarity";
 import fontsStylesheetUrl from "./styles/fonts.css?url";
 import globalStylesheetUrl from "./styles/global.css?url";
+import nProgressCustomStyles from "./styles/nprogress.css?url";
 import styles from "./tailwind.css?url";
 import { ClientHintCheck, getHints } from "./utils/client-hints";
 import { getBrowserEnv } from "./utils/env";
 import { data } from "./utils/http.server";
 import { useNonce } from "./utils/nonce-provider";
 import { splashScreenLinks } from "./utils/splash-screen-links";
-import NProgress from "nprogress";
-import nProgressStyles from "nprogress/nprogress.css?url";
-import { useEffect, useMemo } from "react";
-import { useNavigation } from "@remix-run/react";
-import nProgressCustomStyles from "./styles/nprogress.css?url";
-
 
 export interface RootData {
   env: typeof getBrowserEnv;
