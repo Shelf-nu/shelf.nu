8c57a3778e93bd46a1ee0878c9cfd43b7ce9c221
Bhavya Jain <rishikabhavya@gmail.com>
2024-06-08 19:00:10 +0530

    Initial commit using Intersection Observer

--- app/components/assets/form.tsx
+++ app/components/assets/form.tsx
@@ -115,41 +115,45 @@ export const AssetForm = ({
         message: string;
       };
     };
-  }>(); 
+  }>();
 
   const handleSubmit = () => {
     const scrollToFirstError = () => {
       const errorElements = document.querySelectorAll(".text-error-500");
       if (errorElements.length > 0) {
-          let firstError = errorElements[0];
-          const elementToScrollTo = firstError.previousElementSibling || firstError;
-          elementToScrollTo.scrollIntoView({ behavior: "smooth" });
+        let firstError = errorElements[0];
+        const elementToScrollTo =
+          firstError.previousElementSibling || firstError;
+        elementToScrollTo.scrollIntoView({ behavior: "smooth" });
       }
     };
 
     scrollToFirstError();
 
     const observer = new IntersectionObserver(
-    (entries, observer) => {
-        const notIntersecting = entries.filter(entry => !entry.isIntersecting);
+      (entries, observer) => {
+        const notIntersecting = entries.filter(
+          (entry) => !entry.isIntersecting
+        );
 
         if (notIntersecting.length > 0) {
-        notIntersecting.forEach(entry => {
-            const elementToScrollTo = entry.target.previousElementSibling || entry.target;
+          notIntersecting.forEach((entry) => {
+            const elementToScrollTo =
+              entry.target.previousElementSibling || entry.target;
             elementToScrollTo.scrollIntoView({ behavior: "smooth" });
             observer.unobserve(entry.target);
-        });
+          });
         }
-    },
-    { threshold: 0.1 }
+      },
+      { threshold: 0.1 }
     );
 
     const target = document.body;
 
     return () => {
-    observer.disconnect();
+      observer.disconnect();
     };
-  }
+  };
 
   return (
     <Card className="w-full md:w-min">
