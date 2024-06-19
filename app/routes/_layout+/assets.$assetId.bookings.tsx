a36527335d68d24ce3c2b757f237fee6cd584f4a
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-06-19 08:56:30 +0530

    feat(asset-bookings): create asset booking tab

---
+++ app/routes/_layout+/assets.$assetId.bookings.tsx
@@ -0,0 +1,140 @@
+import { BookingStatus, OrganizationRoles } from "@prisma/client";
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { z } from "zod";
+import type { HeaderData } from "~/components/layout/header/types";
+import { getBookings } from "~/modules/booking/service.server";
+import { setSelectedOrganizationIdCookie } from "~/modules/organization/context.server";
+import { getDateTimeFormat } from "~/utils/client-hints";
+import {
+  setCookie,
+  updateCookieWithPerPage,
+  userPrefs,
+} from "~/utils/cookies.server";
+import { makeShelfError } from "~/utils/error";
+import {
+  data,
+  error,
+  getCurrentSearchParams,
+  getParams,
+} from "~/utils/http.server";
+import { getParamsValues } from "~/utils/list";
+import {
+  PermissionAction,
+  PermissionEntity,
+} from "~/utils/permissions/permission.validator.server";
+import { requirePermission } from "~/utils/roles.server";
+import BookingsIndexPage from "./bookings";
+
+const BOOKING_STATUS_TO_SHOW = [
+  BookingStatus.ARCHIVED,
+  BookingStatus.CANCELLED,
+  BookingStatus.COMPLETE,
+  BookingStatus.ONGOING,
+  BookingStatus.OVERDUE,
+  BookingStatus.RESERVED,
+];
+
+export async function loader({ context, request, params }: LoaderFunctionArgs) {
+  const authSession = context.getSession();
+  const { userId } = authSession;
+
+  const { assetId } = getParams(params, z.object({ assetId: z.string() }), {
+    additionalData: { userId },
+  });
+
+  try {
+    const { organizationId, role } = await requirePermission({
+      userId,
+      request,
+      entity: PermissionEntity.asset,
+      action: PermissionAction.read,
+    });
+
+    const isSelfService = role === OrganizationRoles.SELF_SERVICE;
+
+    const searchParams = getCurrentSearchParams(request);
+    const { page, perPageParam, search, status } =
+      getParamsValues(searchParams);
+
+    const cookie = await updateCookieWithPerPage(request, perPageParam);
+    const { perPage } = cookie;
+
+    const { bookings, bookingCount } = await getBookings({
+      organizationId,
+      page,
+      perPage,
+      search,
+      userId: authSession?.userId,
+      assetIds: [assetId],
+      statuses: status ? [status] : BOOKING_STATUS_TO_SHOW,
+      ...(isSelfService && {
+        // If the user is self service, we only show bookings that belong to that user)
+        custodianUserId: authSession?.userId,
+      }),
+    });
+
+    const totalPages = Math.ceil(bookingCount / perPage);
+
+    const header: HeaderData = {
+      title: "Bookings",
+    };
+    const modelName = {
+      singular: "booking",
+      plural: "bookings",
+    };
+
+    /** We format the dates on the server based on the users timezone and locale  */
+    const items = bookings.map((b) => {
+      if (b.from && b.to) {
+        const from = new Date(b.from);
+        const displayFrom = getDateTimeFormat(request, {
+          dateStyle: "short",
+          timeStyle: "short",
+        }).format(from);
+
+        const to = new Date(b.to);
+        const displayTo = getDateTimeFormat(request, {
+          dateStyle: "short",
+          timeStyle: "short",
+        }).format(to);
+
+        return {
+          ...b,
+          displayFrom: displayFrom.split(","),
+          displayTo: displayTo.split(","),
+        };
+      }
+      return b;
+    });
+
+    return json(
+      data({
+        header,
+        items,
+        search,
+        page,
+        totalItems: bookingCount,
+        totalPages,
+        perPage,
+        modelName,
+      }),
+      {
+        headers: [
+          setCookie(await userPrefs.serialize(cookie)),
+          setCookie(await setSelectedOrganizationIdCookie(organizationId)),
+        ],
+      }
+    );
+  } catch (cause) {
+    const reason = makeShelfError(cause, { userId });
+    throw json(error(reason), { status: reason.status });
+  }
+}
+
+export const handle = {
+  name: "$assetId.bookings",
+};
+
+export default function AssetBookings() {
+  return <BookingsIndexPage className="!mt-0" />;
+}
