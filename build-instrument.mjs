c6dd19488875b3a2f6d0a1438f611d5a3fd1f0cf
Donkoko <nbonev@duck.com>
2024-06-26 14:11:20 +0300

    using ts for instrument file + implementing a custom build for it

---
+++ build-instrument.mjs
@@ -0,0 +1,12 @@
+import { build } from "esbuild";
+
+build({
+  entryPoints: ["./server/instrument.sentry.server.ts"],
+  outfile: "./build/server/instrument.server.mjs",
+  platform: "node",
+  format: "esm",
+  target: "es2022",
+  bundle: true,
+  minify: true,
+  logLevel: "info",
+}).catch(() => process.exit(1));
