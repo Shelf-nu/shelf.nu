d5a3bf89fe0f786e5e398727dc1336bb25b37ddb
Nikolay Bonev <DonKoko@users.noreply.github.com>
2024-06-26 20:03:48 +0200

    Update index.ts
    Added extra clarification comment about importing js file

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
-// // import the Sentry instrumentation file before anything else.
+// import the Sentry instrumentation file before anything else.
+// It is important to import it as .js for this to work, even if the file is .ts
 import "./instrument.server.js";
 
 import { serve } from "@hono/node-server";
