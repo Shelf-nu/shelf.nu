9dc10e8a83c9dd0722be519c4d44adad8b73a630
Donkoko <nbonev@duck.com>
2024-07-19 18:06:38 +0300

    fix svg

--- app/components/icons/library.tsx
+++ app/components/icons/library.tsx
@@ -801,9 +801,9 @@ export function MapIcon(props: SVGProps<SVGSVGElement>) {
     >
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="1.667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.667"
         d="m6.667 2.5 6.666 15M2.5 14.167 10 10m-3.5 7.5h7c1.4 0 2.1 0 2.635-.273a2.5 2.5 0 0 0 1.092-1.092c.273-.535.273-1.235.273-2.635v-7c0-1.4 0-2.1-.273-2.635a2.5 2.5 0 0 0-1.092-1.093C15.6 2.5 14.9 2.5 13.5 2.5h-7c-1.4 0-2.1 0-2.635.272a2.5 2.5 0 0 0-1.093 1.093C2.5 4.4 2.5 5.1 2.5 6.5v7c0 1.4 0 2.1.272 2.635a2.5 2.5 0 0 0 1.093 1.092C4.4 17.5 5.1 17.5 6.5 17.5Z"
       />
     </svg>
