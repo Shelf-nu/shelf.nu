a4525c3bf189568311f1e557f2a212c15b4d7461
Donkoko <nbonev@duck.com>
2024-07-19 17:22:17 +0300

    update docs for id()

--- app/utils/id.server.ts
+++ app/utils/id.server.ts
@@ -4,14 +4,12 @@ import { FINGERPRINT } from "./env";
 import { ShelfError } from "./error";
 import { Logger } from "./logger";
 
-export function id(
-  /**
-   * Default value: 10
-   * Default cuid length: 24
-   * Min cuid length: 7
-   * */
-  length?: number
-) {
+/**
+ * Generate a unique id using cuid2
+ * @param length - The length of the id. Default is 10. Min is 7. Max is 24.
+ * @returns A unique id
+ * */
+export function id(length?: number) {
   try {
     if (length && length < 7) {
       Logger.error(
