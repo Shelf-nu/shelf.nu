1ab11e07d6b25e5f435e6d93d68010018f48c6dd
Nikolay Bonev <nbonev@duck.com>
2024-09-03 17:27:51 +0300

    implementing our own drawer

--- app/components/booking/scanned-assets-drawer.tsx
+++ app/components/booking/scanned-assets-drawer.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { AssetStatus } from "@prisma/client";
 import { Form, useLoaderData } from "@remix-run/react";
+import { motion } from "framer-motion";
 import { useAtomValue, useSetAtom } from "jotai";
+import { createPortal } from "react-dom";
 import { useZorm } from "react-zorm";
 import { z } from "zod";
 import {
@@ -18,15 +20,7 @@ import { AvailabilityLabel } from "./availability-label";
 import { AssetLabel } from "../icons/library";
 import { ListHeader } from "../list/list-header";
 import { Button } from "../shared/button";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-} from "../shared/drawer";
+
 import { Table, Td, Th } from "../table";
 import When from "../when/when";
 
@@ -40,8 +34,11 @@ export const addScannedAssetsToBookingSchema = z.object({
   assetIds: z.array(z.string()).min(1),
 });
 
-const DRAWER_OFFSET = 390;
+/** Used for calculating expanded size */
 const TOP_GAP = 80 + 53 + 8 + 16;
+
+const Portal = ({ children }: { children: React.ReactNode }) =>
+  createPortal(children, document.body);
 
 export default function ScannedAssetsDrawer({
   className,
@@ -50,32 +47,18 @@ export default function ScannedAssetsDrawer({
 }: ScannedAssetsDrawerProps) {
   const { booking } = useLoaderData<typeof loader>();
 
-  const { vh } = useViewportHeight();
-
   const zo = useZorm(
     "AddScannedAssetsToBooking",
     addScannedAssetsToBookingSchema
   );
 
-  /**
-   * The values represent the size of the drawer in pixels based on its content
-   * At snap point 1 we need to show 1 item
-   * At snap point 2 we need to show the full list
-   */
-  const SNAP_POINTS = [`157px`, 1];
-
-  const [snap, setSnap] = useState<number | string | null>(SNAP_POINTS[0]);
+  const [expanded, setExpanded] = useState(false);
+  const { vh } = useViewportHeight();
 
   const fetchedScannedAssets = useAtomValue(fetchedScannedAssetsAtom);
   const fetchedScannedAssetsCount = useAtomValue(fetchedScannedAssetsCountAtom);
   const removeFetchedScannedAsset = useSetAtom(removeFetchedScannedAssetAtom);
   const clearFetchedScannedAssets = useSetAtom(clearFetchedScannedAssetsAtom);
-
-  useEffect(() => {
-    if (fetchedScannedAssetsCount === 0) {
-      setSnap(SNAP_POINTS[0]);
-    }
-  }, [fetchedScannedAssetsCount, setSnap, SNAP_POINTS]);
 
   const displayQrNotification = useSetAtom(displayQrScannerNotificationAtom);
 
@@ -86,147 +69,178 @@ export default function ScannedAssetsDrawer({
     (asset) => asset.status === AssetStatus.IN_CUSTODY
   );
 
+  // Handler for the drag end event
   return (
-    <div>
-      <div className={tw("", className)} style={style}>
-        <div className="sr-only">Add assets to booking via scan</div>
+    <Portal>
+      <motion.div
+        className={tw(
+          "fixed inset-x-0 bottom-0 rounded-t-3xl border bg-white transition-all duration-300 ease-in-out"
+        )}
+        style={{
+          height: expanded ? vh - TOP_GAP : "148px",
+        }}
+        drag="y"
+        dragConstraints={{ top: 0, bottom: 0 }}
+        onDragEnd={(_, info) => {
+          const shouldExpand = info.offset.y < 0;
+          setExpanded(shouldExpand);
+        }}
+      >
+        <div className={tw("", className)} style={style}>
+          <div className="sr-only">Add assets to booking via scan</div>
 
-        <div className="mx-auto size-full px-4 md:max-w-4xl md:px-0">
-          <div className="flex items-center justify-between border-b text-left">
-            <div>{fetchedScannedAssetsCount} assets scanned</div>
-
-            <When truthy={fetchedScannedAssetsCount > 0}>
-              <DrawerDescription
-                className="cursor-pointer"
-                onClick={clearFetchedScannedAssets}
-              >
-                Clear list
-              </DrawerDescription>
-            </When>
-          </div>
-
-          <When truthy={fetchedScannedAssetsCount === 0}>
-            <div className="my-16 flex flex-col items-center px-3 text-center">
-              <div className="mb-4 rounded-full bg-primary-50  p-2">
-                <div className=" rounded-full bg-primary-100 p-2 text-primary">
-                  <AssetLabel className="size-6" />
-                </div>
-              </div>
-
-              <div>
-                <div className="text-base font-semibold text-gray-900">
-                  List is empty
-                </div>
-                <p className="text-sm text-gray-600">
-                  Fill list by scanning codes...
-                </p>
-              </div>
+          <div className="mx-auto size-full px-4 md:max-w-4xl md:px-0">
+            <div
+              className="border-b py-1 text-center hover:cursor-grab"
+              onClick={() => {
+                setExpanded((prev) => !prev);
+              }}
+            >
+              {/* Drag me */}
+              <motion.div className="mx-auto my-1 h-2 w-[30px] rounded-full bg-gray-500/50" />
             </div>
-          </When>
+            <div className="flex items-center justify-between border-b text-left">
+              <div className="py-4">
+                {fetchedScannedAssetsCount} assets scanned
+              </div>
 
-          <When truthy={fetchedScannedAssetsCount > 0}>
-            <div className="h-[600px] overflow-auto">
-              <Table className="overflow-y-auto">
-                <ListHeader hideFirstColumn>
-                  <Th className="p-0"> </Th>
-                  <Th className="p-0"> </Th>
-                </ListHeader>
+              <When truthy={fetchedScannedAssetsCount > 0}>
+                <div
+                  className="cursor-pointer"
+                  onClick={clearFetchedScannedAssets}
+                >
+                  Clear list
+                </div>
+              </When>
+            </div>
+            <When truthy={fetchedScannedAssetsCount === 0}>
+              <div className="flex flex-col items-center px-3 py-6 text-center">
+                {expanded && (
+                  <div className="mb-4 rounded-full bg-primary-50  p-2">
+                    <div className=" rounded-full bg-primary-100 p-2 text-primary">
+                      <AssetLabel className="size-6" />
+                    </div>
+                  </div>
+                )}
 
-                <tbody>
-                  {fetchedScannedAssets.map((asset) => (
-                    <Tr key={asset.id}>
-                      <Td className="w-full p-0 md:p-0">
-                        <div className="flex items-center justify-between gap-3 p-4 md:px-6">
-                          <div className="flex items-center gap-3">
-                            <div className="flex flex-col gap-y-1">
-                              <p className="word-break whitespace-break-spaces font-medium">
-                                {asset.title}
-                              </p>
+                <div>
+                  {expanded && (
+                    <div className="text-base font-semibold text-gray-900">
+                      List is empty
+                    </div>
+                  )}
 
-                              <div className="flex flex-wrap items-center gap-x-2 gap-y-1">
-                                <AvailabilityLabel
-                                  isAddedThroughKit={
-                                    booking.assets.some(
+                  <p className="text-sm text-gray-600">
+                    Fill list by scanning codes...
+                  </p>
+                </div>
+              </div>
+            </When>
+            <When truthy={fetchedScannedAssetsCount > 0}>
+              <div className="h-[600px] overflow-auto">
+                <Table className="overflow-y-auto">
+                  <ListHeader hideFirstColumn>
+                    <Th className="p-0"> </Th>
+                    <Th className="p-0"> </Th>
+                  </ListHeader>
+
+                  <tbody>
+                    {fetchedScannedAssets.map((asset) => (
+                      <Tr key={asset.id}>
+                        <Td className="w-full p-0 md:p-0">
+                          <div className="flex items-center justify-between gap-3 p-4 md:px-6">
+                            <div className="flex items-center gap-3">
+                              <div className="flex flex-col gap-y-1">
+                                <p className="word-break whitespace-break-spaces font-medium">
+                                  {asset.title}
+                                </p>
+
+                                <div className="flex flex-wrap items-center gap-x-2 gap-y-1">
+                                  <AvailabilityLabel
+                                    isAddedThroughKit={
+                                      booking.assets.some(
+                                        (a) => a.id === asset.id
+                                      ) && !!asset.kitId
+                                    }
+                                    isAlreadyAdded={booking.assets.some(
                                       (a) => a.id === asset.id
-                                    ) && !!asset.kitId
-                                  }
-                                  isAlreadyAdded={booking.assets.some(
-                                    (a) => a.id === asset.id
-                                  )}
-                                  showKitStatus
-                                  asset={asset}
-                                  isCheckedOut={asset.status === "CHECKED_OUT"}
-                                />
+                                    )}
+                                    showKitStatus
+                                    asset={asset}
+                                    isCheckedOut={
+                                      asset.status === "CHECKED_OUT"
+                                    }
+                                  />
+                                </div>
                               </div>
                             </div>
                           </div>
-                        </div>
-                      </Td>
-                      <Td>
-                        <Button
-                          className="border-none"
-                          variant="ghost"
-                          icon="trash"
-                          onClick={() => {
-                            removeFetchedScannedAsset(asset.id);
-                            displayQrNotification({
-                              message: "Asset was removed from list",
-                            });
-                          }}
-                        />
-                      </Td>
-                    </Tr>
-                  ))}
-                </tbody>
-              </Table>
-            </div>
-          </When>
-
-          <When truthy={fetchedScannedAssetsCount > 0}>
-            <div>
-              <When truthy={!!zo.errors.assetIds()?.message}>
-                <p className="text-sm text-error-500">
-                  {zo.errors.assetIds()?.message}
-                </p>
-              </When>
-
-              <div className="flex-row px-0">
-                <div asChild>
-                  <Button
-                    type="button"
-                    variant="outline"
-                    className="w-full max-w-full"
-                    disabled={isLoading}
-                  >
-                    Close
-                  </Button>
-                </div>
-
-                <Form ref={zo.ref} className="w-full" method="POST">
-                  {fetchedScannedAssets.map((asset, i) => (
-                    <input
-                      key={asset.id}
-                      type="hidden"
-                      name={`assetIds[${i}]`}
-                      value={asset.id}
-                    />
-                  ))}
-
-                  <Button
-                    className="w-full max-w-full"
-                    disabled={
-                      isLoading || someAssetsCheckedOut || someAssetsInCustody
-                    }
-                  >
-                    Confirm
-                  </Button>
-                </Form>
+                        </Td>
+                        <Td>
+                          <Button
+                            className="border-none"
+                            variant="ghost"
+                            icon="trash"
+                            onClick={() => {
+                              removeFetchedScannedAsset(asset.id);
+                              displayQrNotification({
+                                message: "Asset was removed from list",
+                              });
+                            }}
+                          />
+                        </Td>
+                      </Tr>
+                    ))}
+                  </tbody>
+                </Table>
               </div>
-            </div>
-          </When>
+            </When>
+            <When truthy={fetchedScannedAssetsCount > 0}>
+              <div>
+                <When truthy={!!zo.errors.assetIds()?.message}>
+                  <p className="text-sm text-error-500">
+                    {zo.errors.assetIds()?.message}
+                  </p>
+                </When>
+
+                <div className="flex-row px-0">
+                  <div>
+                    <Button
+                      type="button"
+                      variant="outline"
+                      className="w-full max-w-full"
+                      disabled={isLoading}
+                    >
+                      Close
+                    </Button>
+                  </div>
+
+                  <Form ref={zo.ref} className="w-full" method="POST">
+                    {fetchedScannedAssets.map((asset, i) => (
+                      <input
+                        key={asset.id}
+                        type="hidden"
+                        name={`assetIds[${i}]`}
+                        value={asset.id}
+                      />
+                    ))}
+
+                    <Button
+                      className="w-full max-w-full"
+                      disabled={
+                        isLoading || someAssetsCheckedOut || someAssetsInCustody
+                      }
+                    >
+                      Confirm
+                    </Button>
+                  </Form>
+                </div>
+              </div>
+            </When>
+          </div>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </Portal>
   );
 }
 
