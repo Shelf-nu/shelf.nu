e023233e71749acec208d4239923c342dcd6c531
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-11-26 20:42:22 +0100

    feat(improve-404): handling special case of multiple organization for team member

--- app/components/errors/error-404-handler.tsx
+++ app/components/errors/error-404-handler.tsx
@@ -1,8 +1,16 @@
+import { useMemo } from "react";
 import { tw } from "~/utils/tw";
 import { Error404AdditionalData } from "./utils";
 import { Button } from "../shared/button";
 import { useFetcher } from "@remix-run/react";
 import { isFormProcessing } from "~/utils/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../forms/select";
 
 export type Error404HandlerProps = {
   className?: string;
@@ -18,44 +26,110 @@ export default function Error404Handler({
   const fetcher = useFetcher();
   const disabled = isFormProcessing(fetcher.state);
 
+  const content = useMemo(() => {
+    switch (additionalData.model) {
+      case "asset":
+      case "kit":
+      case "location": {
+        return (
+          <div className="flex flex-col items-center text-center">
+            <div className="w-full md:max-w-screen-sm">
+              <h2 className="mb-2">
+                <span className="capitalize">{additionalData.model}</span>{" "}
+                belongs to another workspace.
+              </h2>
+              <p className="mb-4">
+                The {additionalData.model} you are trying to view belongs to a
+                different workspace you are part of. Would you like to switch to
+                workspace{" "}
+                <span className="font-bold">
+                  "{additionalData.organization.organization.name}"
+                </span>{" "}
+                to view the {additionalData.model}?
+              </p>
+              <fetcher.Form
+                action="/api/user/change-current-organization"
+                method="POST"
+              >
+                <input
+                  type="hidden"
+                  name="organizationId"
+                  value={additionalData.organization.organization.id}
+                />
+                <input
+                  type="hidden"
+                  name="redirectTo"
+                  value={additionalData.redirectTo}
+                />
+                <Button disabled={disabled}>Switch workspace</Button>
+              </fetcher.Form>
+            </div>
+          </div>
+        );
+      }
+
+      case "teamMember": {
+        return (
+          <div className="flex flex-col items-center text-center">
+            <div className="w-full md:max-w-screen-sm">
+              <h2 className="mb-2">
+                <span className="capitalize">Team Member</span> belongs to
+                another workspace(s).
+              </h2>
+              <p className="mb-4">
+                The team member you are trying to view belongs to one/some of
+                your different workspace you are part of. Would you like to
+                switch to workspace to view the team member?
+              </p>
+              <fetcher.Form
+                action="/api/user/change-current-organization"
+                method="POST"
+                className="flex items-center flex-col"
+              >
+                <Select name="organizationId" disabled={disabled}>
+                  <SelectTrigger className="mb-4 px-3.5 py-2 text-left text-gray-500 max-w-80">
+                    <SelectValue placeholder="Select organization to switch" />
+                  </SelectTrigger>
+                  <SelectContent
+                    position="popper"
+                    className="w-full min-w-80 overflow-auto p-1"
+                    align="start"
+                  >
+                    {additionalData.organizations.map(({ organization }) => (
+                      <SelectItem
+                        value={organization.id}
+                        key={organization.id}
+                        className="px-4 py-2"
+                      >
+                        {organization.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <input
+                  type="hidden"
+                  name="redirectTo"
+                  value={additionalData.redirectTo}
+                />
+                <Button disabled={disabled}>Switch workspace</Button>
+              </fetcher.Form>
+            </div>
+          </div>
+        );
+      }
+
+      default: {
+        return null;
+      }
+    }
+  }, [additionalData]);
+
   return (
     <div
       className={tw("flex size-full items-center justify-center", className)}
       style={style}
     >
-      <div className="flex flex-col items-center text-center">
-        <div className="w-full md:max-w-screen-sm">
-          <h2 className="mb-2">
-            <span className="capitalize">{additionalData.model}</span> belongs
-            to another workspace.
-          </h2>
-          <p className="mb-4">
-            The {additionalData.model} you are trying to view belongs to a
-            different workspace you are part of. Would you like to switch to
-            workspace{" "}
-            <span className="font-bold">
-              "{additionalData.organization.organization.name}"
-            </span>{" "}
-            to view the {additionalData.model}?
-          </p>
-          <fetcher.Form
-            action="/api/user/change-current-organization"
-            method="POST"
-          >
-            <input
-              type="hidden"
-              name="organizationId"
-              value={additionalData.organization.organization.id}
-            />
-            <input
-              type="hidden"
-              name="redirectTo"
-              value={additionalData.redirectTo}
-            />
-            <Button disabled={disabled}>Switch workspace</Button>
-          </fetcher.Form>
-        </div>
-      </div>
+      {content}
     </div>
   );
 }
