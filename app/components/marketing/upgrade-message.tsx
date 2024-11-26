52a6b5426c2e34c9c2503dd54595d61550d68ae0
Donkoko <nbonev@duck.com>
2024-11-26 14:34:31 +0200

    fix: correct export permissions - make sure free users cannot use premium feature

--- app/components/marketing/upgrade-message.tsx
+++ app/components/marketing/upgrade-message.tsx
@@ -4,7 +4,11 @@ export function UpgradeMessage() {
   return (
     <>
       Please consider{" "}
-      <Button to="/account-details/subscription" variant="link">
+      <Button
+        to="/account-details/subscription"
+        variant="link"
+        className="!m-0 !p-0"
+      >
         upgrading
       </Button>{" "}
       your subscription to access this feature.
