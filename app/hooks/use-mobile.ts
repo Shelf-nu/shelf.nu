725515d130b7eaa52808e97a90bb9efc2cf34ac6
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-12-09 16:11:34 +0100

    refactor(sidebar): update use-mobile hook

--- app/hooks/use-mobile.ts
+++ app/hooks/use-mobile.ts
@@ -6,17 +6,21 @@ export function useIsMobile() {
   const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined);
 
   useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+    let mql: MediaQueryList;
 
-    const onChange = () => {
+    if (window) {
+      mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+
+      mql.addEventListener("change", onChange);
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    };
+    }
 
-    mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    function onChange() {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    }
 
     return () => {
-      mql.removeEventListener("change", onChange);
+      mql?.removeEventListener("change", onChange);
     };
   }, []);
 
