b98b5382e2bfa21ab792bf891745694bf3079e84
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2025-02-20 10:05:39 +0100

    feat(sign-template): rendering Link instead of <a> in markdown renderer

--- app/utils/md.ts
+++ app/utils/md.ts
@@ -2,5 +2,23 @@ import { parse, transform } from "@markdoc/markdoc";
 import type { Config } from "@markdoc/markdoc";
 
 export function parseMarkdownToReact(markdown: string, options: Config = {}) {
-  return transform(parse(markdown), options);
+  const config = {
+    ...options,
+    nodes: {
+      ...options.nodes,
+      /**
+       * Markdown renders link a native <a> tags by default, which causes a full page reload.
+       * To prevent this, we replace it with Remix's <Link> component for internal links.
+       * External links will still use <a> and open in new tab.
+       */
+      link: {
+        render: "Link",
+        attributes: {
+          href: { type: String, required: true },
+        },
+      },
+    },
+  } satisfies Config;
+
+  return transform(parse(markdown), config);
 }
