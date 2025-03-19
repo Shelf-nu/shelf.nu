cffefbf46a1d0fe6c502ab4ec9d53c48d2742c71
Nikolay Bonev <nbonev@duck.com>
2025-03-19 09:11:29 +0200

    kick-off new design implementation

--- app/components/zxing-scanner/code-scanner.tsx
+++ app/components/zxing-scanner/code-scanner.tsx
@@ -73,12 +73,12 @@ export const CodeScanner = ({
       )}
     >
       <div className="relative size-full overflow-hidden">
-        <div className="absolute inset-x-0 top-0 z-30 flex w-full items-center justify-between bg-transparent text-white">
+        <div className="absolute inset-x-0 top-0 z-30 flex w-full items-center justify-between bg-white px-4 py-2 text-gray-900">
           <div>
             {!hideBackButtonText && (
               <Link
                 to=".."
-                className="inline-flex items-center justify-start p-2 text-[11px] leading-[11px] text-white"
+                className="inline-flex items-center justify-start text-[11px] leading-[11px] "
               >
                 <TriangleLeftIcon className="size-[14px]" />
                 <span className="mt-[-0.5px]">{backButtonText}</span>
