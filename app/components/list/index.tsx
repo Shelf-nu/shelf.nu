726b1b7b35081ba2b84e9294cfffcf0676a41ef8
Donkoko <nbonev@duck.com>
2025-04-07 13:04:25 +0300

    fixing wrong usage of key

--- app/components/list/index.tsx
+++ app/components/list/index.tsx
@@ -22,6 +22,7 @@ import { ExportAssetsButton } from "../assets/assets-index/export-assets-button"
 import { Button } from "../shared/button";
 import { Table } from "../table";
 import When from "../when/when";
+import { generateRandomCode } from "~/modules/invite/helpers";
 
 export interface IndexResponse {
   header: {
@@ -235,7 +236,7 @@ export const List = React.forwardRef<HTMLDivElement, ListProps>(function List(
               children={headerChildren}
               hideFirstColumn={hideFirstHeaderColumn}
             />
-            <tbody key={`item-list-${items.length}`}>
+            <tbody key={`item-list-${items.length}-${generateRandomCode(5)}`}>
               {items.map((item, i) => (
                 <ListItem
                   item={item}
