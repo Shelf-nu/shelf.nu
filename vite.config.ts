e9942d0d6c695d1299220bd08cbb1ca32f15901b
TomStory <tomstory9@gmail.com>
2025-05-21 11:46:29 +0200

    Feat paraglide config and translation file

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { paraglideVitePlugin } from '@inlang/paraglide-js'
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -54,58 +54,63 @@ export default defineConfig({
         "./node_modules/.prisma/client/index-browser.js",
     },
   },
-  plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './app/paraglide', strategy: ["preferredLanguage", "cookie", "baseLocale"] }),
-  cjsInterop({
-    // List of CJS dependencies that require interop
-    dependencies: [
-      "react-microsoft-clarity",
-      "@markdoc/markdoc",
-      "react-to-print",
-    ],
-  }),
-  devServer(),
+  plugins: [
+    paraglideVitePlugin({
+      project: "./project.inlang",
+      outdir: "./app/paraglide",
+      strategy: ["preferredLanguage", "cookie", "baseLocale"],
+    }),
+    cjsInterop({
+      // List of CJS dependencies that require interop
+      dependencies: [
+        "react-microsoft-clarity",
+        "@markdoc/markdoc",
+        "react-to-print",
+      ],
+    }),
+    devServer(),
 
-  remix({
-    ignoredRouteFiles: ["**/.*"],
-    future: {
-      // unstable_optimizeDeps: true,
-    },
-    routes: async (defineRoutes) => {
-      return flatRoutes("routes", defineRoutes);
-    },
+    remix({
+      ignoredRouteFiles: ["**/.*"],
+      future: {
+        // unstable_optimizeDeps: true,
+      },
+      routes: async (defineRoutes) => {
+        return flatRoutes("routes", defineRoutes);
+      },
 
-    buildEnd: async ({ remixConfig }) => {
-      const sentryInstrument = `instrument.server`;
-      await esbuild
-        .build({
-          alias: {
-            "~": `./app`,
-          },
-          outdir: `${remixConfig.buildDirectory}/server`,
-          entryPoints: [`./server/${sentryInstrument}.ts`],
-          platform: "node",
-          format: "esm",
-          // Don't include node_modules in the bundle
-          packages: "external",
-          bundle: true,
-          logLevel: "info",
-        })
-        .then(() => {
-          const serverBuildPath = `${remixConfig.buildDirectory}/server/${remixConfig.serverBuildFile}`;
-          fs.writeFileSync(
-            serverBuildPath,
-            Buffer.concat([
-              Buffer.from(`import "./${sentryInstrument}.js"\n`),
-              Buffer.from(fs.readFileSync(serverBuildPath)),
-            ])
-          );
-        })
-        .catch((error: unknown) => {
-          console.error(error);
-          process.exit(1);
-        });
-    },
-  }),
-  tsconfigPaths(),
+      buildEnd: async ({ remixConfig }) => {
+        const sentryInstrument = `instrument.server`;
+        await esbuild
+          .build({
+            alias: {
+              "~": `./app`,
+            },
+            outdir: `${remixConfig.buildDirectory}/server`,
+            entryPoints: [`./server/${sentryInstrument}.ts`],
+            platform: "node",
+            format: "esm",
+            // Don't include node_modules in the bundle
+            packages: "external",
+            bundle: true,
+            logLevel: "info",
+          })
+          .then(() => {
+            const serverBuildPath = `${remixConfig.buildDirectory}/server/${remixConfig.serverBuildFile}`;
+            fs.writeFileSync(
+              serverBuildPath,
+              Buffer.concat([
+                Buffer.from(`import "./${sentryInstrument}.js"\n`),
+                Buffer.from(fs.readFileSync(serverBuildPath)),
+              ])
+            );
+          })
+          .catch((error: unknown) => {
+            console.error(error);
+            process.exit(1);
+          });
+      },
+    }),
+    tsconfigPaths(),
   ],
 });
