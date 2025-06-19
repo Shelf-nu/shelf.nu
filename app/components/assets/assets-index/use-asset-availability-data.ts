2f3bd2caf342d831609adeea102088977019eed6
Donkoko <nbonev@duck.com>
2025-06-19 12:50:45 +0300

    creating custom fix for nowIndicator issue

--- app/components/assets/assets-index/use-asset-availability-data.ts
+++ app/components/assets/assets-index/use-asset-availability-data.ts
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import type { Booking, TeamMember, User } from "@prisma/client";
+import type { SerializeFrom } from "@remix-run/node";
 import { useCurrentOrganization } from "~/hooks/use-current-organization";
 import { useUserRoleHelper } from "~/hooks/user-user-role-helper";
 import type { AssetIndexLoaderData } from "~/routes/_layout+/assets._index";
@@ -9,7 +10,10 @@ import { toIsoDateTimeToUserTimezone } from "~/utils/date-fns";
 import type { OrganizationPermissionSettings } from "~/utils/permissions/custody-and-bookings-permissions.validator.client";
 import { userHasCustodyViewPermission } from "~/utils/permissions/custody-and-bookings-permissions.validator.client";
 
-export function useAssetAvailabilityData(items: AssetIndexLoaderData["items"]) {
+type LoaderData = SerializeFrom<AssetIndexLoaderData>;
+type Items = NonNullable<LoaderData["items"]>;
+
+export function useAssetAvailabilityData(items: Items) {
   const { roles } = useUserRoleHelper();
   const organization = useCurrentOrganization();
   const canSeeAllCustody = userHasCustodyViewPermission({
