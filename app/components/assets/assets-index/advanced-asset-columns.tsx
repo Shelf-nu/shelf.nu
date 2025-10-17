43222acea1507ad0c2c5ce6cb3c0286c2f3ec1d6
Carlos Virreira <carlos@whale-agency.com>
2025-10-17 13:35:57 +0200

    feat: reveal overflow asset tags with tooltip

--- app/components/assets/assets-index/advanced-asset-columns.tsx
+++ app/components/assets/assets-index/advanced-asset-columns.tsx
@@ -63,7 +63,7 @@ import { AssetImage } from "../asset-image/component";
 import { AssetStatusBadge } from "../asset-status-badge";
 import AssetQuickActions from "./asset-quick-actions";
 // eslint-disable-next-line import/no-cycle
-import { ListItemTagsColumn } from "./assets-list";
+import { ListItemTagsColumn } from "./list-item-tags-column";
 import { CategoryBadge } from "../category-badge";
 
 export function AdvancedIndexColumn({
---
+++ app/components/assets/assets-index/list-item-tags-column.tsx
@@ -0,0 +1,24 @@
+import type { Tag } from "@prisma/client";
+
+import ItemsWithViewMore from "~/components/list/items-with-view-more";
+import { Tag as TagBadge } from "~/components/shared/tag";
+
+export const ListItemTagsColumn = ({
+  tags,
+}: {
+  tags: Pick<Tag, "id" | "name">[] | undefined;
+}) => {
+  if (!tags || tags.length === 0) {
+    return null;
+  }
+
+  return (
+    <ItemsWithViewMore
+      items={tags}
+      showCount={2}
+      className="justify-start text-left"
+      renderItem={(tag) => <TagBadge key={tag.id}>{tag.name}</TagBadge>}
+      emptyMessage={null}
+    />
+  );
+};
